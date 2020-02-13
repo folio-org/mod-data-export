6a6b465068288685aefba1f267326c1f589badef
igor_gorchakov <Igor_Gorchakov@epam.com>
2020-02-13 15:36:53 +0200

    Optimized imports

--- src/main/java/org/folio/dao/impl/FileDefinitionDaoImpl.java
+++ src/main/java/org/folio/dao/impl/FileDefinitionDaoImpl.java
@@ -2,11 +2,8 @@ package org.folio.dao.impl;
 
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.sql.ResultSet;
 import io.vertx.ext.sql.UpdateResult;
 import org.folio.dao.FileDefinitionDao;
 import org.folio.rest.jaxrs.model.FileDefinition;
@@ -15,16 +12,7 @@ import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.interfaces.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import javax.ws.rs.NotFoundException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
 import java.util.Optional;
-
-import static java.lang.String.format;
-import static org.folio.rest.persist.PostgresClient.convertToPsqlStandard;
 
 @Repository
 public class FileDefinitionDaoImpl implements FileDefinitionDao {
