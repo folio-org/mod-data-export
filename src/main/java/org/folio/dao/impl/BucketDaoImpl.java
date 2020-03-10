ab95d971d8f37f881635a7b19aae67d5de87e94f
Dmytro Popov <dmytro_popov@epam.com>
2020-03-10 15:53:16 +0200

    MDEXP-68: Use promise

--- src/main/java/org/folio/dao/impl/BucketDaoImpl.java
+++ src/main/java/org/folio/dao/impl/BucketDaoImpl.java
@@ -4,6 +4,7 @@ import java.lang.invoke.MethodHandles;
 import java.util.Optional;
 import java.util.UUID;
 
+import io.vertx.core.Promise;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.dao.BucketDao;
 import org.folio.rest.jaxrs.model.Bucket;
@@ -27,25 +28,25 @@ public class BucketDaoImpl implements BucketDao {
 
   @Override
   public Future<Optional<Bucket>> getByTenantId(String tenantId) {
-    Future<Results<Bucket>> future = Future.future();
+    Promise<Results<Bucket>> promise = Promise.promise();
     try {
-      pgClientFactory.getInstance(tenantId).get(TABLE_NAME, Bucket.class, new Criterion(), false, false, future.completer());
+      pgClientFactory.getInstance(tenantId).get(TABLE_NAME, Bucket.class, new Criterion(), false, false, promise);
     } catch (Exception e) {
       LOGGER.error("Error getting S3 bucket for tenant", e);
-      future.fail(e);
+      promise.fail(e);
     }
-    return future
+    return promise.future()
       .map(Results::getResults)
       .map(r -> r.isEmpty() ? Optional.empty() : Optional.of(r.get(0)));
   }
 
   @Override
   public Future<String> save(String tenantId, Bucket tenantBucket) {
-    Future<String> future = Future.future();
+    Promise<String> promise = Promise.promise();
     if (StringUtils.isEmpty(tenantBucket.getId())) {
       tenantBucket.setId(UUID.randomUUID().toString());
     }
-    pgClientFactory.getInstance(tenantId).save(TABLE_NAME, tenantBucket, future.completer());
-    return future;
+    pgClientFactory.getInstance(tenantId).save(TABLE_NAME, tenantBucket, promise);
+    return promise.future();
   }
 }
