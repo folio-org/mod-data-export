800674bbe23191e3e7861fcf919ed3d7816df147
Illia Borysenko <Illia_Borysenko@epam.com>
2020-03-11 17:36:17 +0200

    Fix SourceReader to start close stream in InputDataManager

--- src/main/java/org/folio/service/manager/inputdatamanager/reader/LocalStorageCsvSourceReader.java
+++ src/main/java/org/folio/service/manager/inputdatamanager/reader/LocalStorageCsvSourceReader.java
@@ -15,18 +15,41 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.Objects.nonNull;
+
 @SuppressWarnings({"java:S2095"})
 @Service
 public class LocalStorageCsvSourceReader implements SourceReader {
   private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+  private Stream<String> fileStream;
+  private Iterator<List<String>> iterator;
+
   @Override
-  public Iterator<List<String>> getFileContentIterator(FileDefinition fileDefinition, int batchSize) {
-    try(Stream<String> lines = Files.lines(Paths.get(fileDefinition.getSourcePath()))) {
-      return Iterables.partition(lines::iterator, batchSize).iterator();
+  public void init(FileDefinition fileDefinition, int batchSize) {
+    try{
+      fileStream = Files.lines(Paths.get(fileDefinition.getSourcePath()));
+      iterator = Iterables.partition(fileStream::iterator, batchSize).iterator();
     } catch (IOException e) {
       LOGGER.error("Exception while reading from {} ", fileDefinition.getFileName(), e);
+      iterator = Collections.emptyIterator();
     }
-    return Collections.emptyIterator();
+  }
+
+  @Override
+  public boolean hasNext() {
+    return iterator.hasNext();
+  }
+
+  @Override
+  public List<String> readNext() {
+    return iterator.next();
+  }
+
+  @Override
+  public void close() {
+    if(nonNull(fileStream)) {
+      fileStream.close();
+    }
   }
 }
