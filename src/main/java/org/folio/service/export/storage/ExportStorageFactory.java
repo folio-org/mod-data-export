7463d6d731ddc783a4cf2bdfd4bc0781dc82e683
Kruthi vuppala <kvuppala@ebsco.com>
2020-03-10 23:57:21 -0400

    refactor

--- src/main/java/org/folio/service/export/storage/ExportStorageFactory.java
+++ src/main/java/org/folio/service/export/storage/ExportStorageFactory.java
@@ -1,35 +1,16 @@
 package org.folio.service.export.storage;
 
-import java.lang.invoke.MethodHandles;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExportStorageFactory {
-  private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-
+  /**
+   * Currently supports only Amazon S3, more implementations will be added
+   * @return
+   */
   public ExportStorageService getExportStorageImplementation() {
-    ExportStorageService ret;
-
-    String type = System.getProperty("file.storage");
-    if (type == null)
-      type = "";
-
-    //add additional implementations later
-    switch (type) {
-    case "AWSS3":
-      ret = new AWSStorageServiceImpl();
-      break;
-    default:
-      ret = new AWSStorageServiceImpl();
-    }
-
-
-    LOGGER.info(String.format("type: %s, class: %s", type, ret.getClass()
-      .getName()));
-    return ret;
+    return new AWSStorageServiceImpl();
   }
 
 }
