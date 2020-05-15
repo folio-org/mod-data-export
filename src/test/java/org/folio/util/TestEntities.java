4ac9618b345f9b6a9cbd26ab2c7fed06883675ea
Kruthi vuppala <kvuppala@ebsco.com>
2020-05-15 10:00:21 -0400

    correcting typo

--- src/test/java/org/folio/util/TestEntities.java
+++ src/test/java/org/folio/util/TestEntities.java
@@ -5,7 +5,7 @@ import org.folio.rest.jaxrs.model.JobProfile;
 import org.folio.rest.jaxrs.model.MappingProfile;
 
 public enum TestEntities {
-  MAPPINGPROFILE("/data-export/mappingProfiles", MappingProfile.class, "profiles/MappingProfile.json", "description", "Testing Export", 0),
+  MAPPINGPROFILE("/data-export/mappingProfiles", MappingProfile.class, "profiles/mappingProfile.json", "description", "Testing Export", 0),
   JOBPROFILE("/data-export/jobProfiles", JobProfile.class, "profiles/jobProfile.json", "description", "Testing Export", 0);
 
 
