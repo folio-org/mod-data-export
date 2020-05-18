76bf7a99936f796635b762fe5e34b0d14fc33119
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-05-18 16:20:24 +0300

    MDEXP-131 Change entities crud tests

--- src/test/java/org/folio/rest/impl/EntitiesCrudTest.java
+++ src/test/java/org/folio/rest/impl/EntitiesCrudTest.java
@@ -13,6 +13,7 @@ import kotlin.text.Charsets;
 import org.apache.commons.io.FileUtils;
 import org.folio.TestUtil;
 import org.folio.rest.RestVerticleTestBase;
+import org.folio.rest.jaxrs.model.MappingProfile;
 import org.folio.util.TestEntities;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -38,11 +39,19 @@ class EntitiesCrudTest extends RestVerticleTestBase{
   @EnumSource(TestEntities.class)
   void testVerifyCollection(TestEntities testEntity) throws MalformedURLException {
     logger.info(String.format("--- mod-data-export %s test: Verifying database's initial state ... ", testEntity.name()));
-    getRequest(testEntity.getEndpoint()).then()
-      .log()
-      .all()
-      .statusCode(200)
-      .body("totalRecords", equalTo(0));
+    if (testEntity.getClazz().getName().equals(MappingProfile.class.getName())) {
+      getRequest(testEntity.getEndpoint()).then()
+        .log()
+        .all()
+        .statusCode(200)
+        .body("totalRecords", equalTo(1));
+    } else {
+      getRequest(testEntity.getEndpoint()).then()
+        .log()
+        .all()
+        .statusCode(200)
+        .body("totalRecords", equalTo(0));
+    }
   }
 
   @ParameterizedTest
@@ -65,12 +74,19 @@ class EntitiesCrudTest extends RestVerticleTestBase{
   @EnumSource(TestEntities.class)
   void testVerifyCollectionQuantity(TestEntities testEntity) throws MalformedURLException {
     logger.info(String.format("--- mod-data-export %s test: Verifying only 1 record was created ... ", testEntity.name()));
-    getRequest(testEntity.getEndpoint()).then()
-    .log()
-    .all()
-    .statusCode(200)
-    .body("totalRecords", equalTo(1));
-
+    if (testEntity.getClazz().getName().equals(MappingProfile.class.getName())) {
+      getRequest(testEntity.getEndpoint()).then()
+        .log()
+        .all()
+        .statusCode(200)
+        .body("totalRecords", equalTo(2));
+    } else {
+      getRequest(testEntity.getEndpoint()).then()
+        .log()
+        .all()
+        .statusCode(200)
+        .body("totalRecords", equalTo(1));
+    }
   }
 
   @ParameterizedTest
