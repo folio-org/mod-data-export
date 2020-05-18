15c2398b9b739cf5c69130c7ea381e6b2d9e691d
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-05-18 15:27:34 +0300

    MDEXP-131 Create default mapping profile

--- src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -2,25 +2,38 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.TenantAttributes;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.utils.TenantTool;
 import org.folio.service.file.cleanup.StorageCleanupService;
 import org.folio.spring.SpringContextUtil;
 import org.folio.util.OkapiConnectionParams;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 public class ModTenantAPI extends TenantAPI {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ModTenantAPI.class);
 
   private static final long DELAY_TIME_BETWEEN_CLEANUP_VALUE_MILLIS = 3600_000;
-
-  private static final Logger logger = LoggerFactory.getLogger(ModTenantAPI.class);
+  private static final String X_OKAPI_TENANT = "x-okapi-tenant";
+  private static final String DEFAULT_MAPPING_PROFILE_SQL = "templates/db_scripts/default_mapping_profile.sql";
+  private static final String TENANT_PLACEHOLDER = "${myuniversity}";
+  private static final String MODULE_PLACEHOLDER = "${mymodule}";
 
   @Autowired
   private StorageCleanupService storageCleanupService;
@@ -31,13 +44,14 @@ public class ModTenantAPI extends TenantAPI {
 
   @Validate
   @Override
-  public void postTenant(TenantAttributes entity, Map<String, String> headers, Handler<AsyncResult<Response>> handlers, Context context)  {
+  public void postTenant(TenantAttributes entity, Map<String, String> headers, Handler<AsyncResult<Response>> handlers, Context context) {
     super.postTenant(entity, headers, asyncResult -> {
       if (asyncResult.failed()) {
         handlers.handle(asyncResult);
       } else {
         initStorageCleanupService(headers, context);
-        handlers.handle(asyncResult);
+        Future<List<String>> sampleData = setupTestData(DEFAULT_MAPPING_PROFILE_SQL, headers, context);
+        sampleData.setHandler(event -> handlers.handle(asyncResult));
       }
     }, context);
   }
@@ -52,11 +66,41 @@ public class ModTenantAPI extends TenantAPI {
     vertx.<Void>executeBlocking(blockingCodeHandler -> storageCleanupService.cleanStorage(params),
       cleanupAsyncResult -> {
         if (cleanupAsyncResult.failed()) {
-          logger.error("Error during cleaning file storage.", cleanupAsyncResult.cause().getMessage());
+          LOGGER.error("Error during cleaning file storage.", cleanupAsyncResult.cause().getMessage());
         } else {
-          logger.info("File storage was successfully cleaned of unused files");
+          LOGGER.info("File storage was successfully cleaned of unused files");
         }
       });
+  }
+
+  private Future<List<String>> setupTestData(String script, Map<String, String> headers, Context context) {
+    try {
+      InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(script);
+
+      if (inputStream == null) {
+        LOGGER.info("Default data was not initialized: no resources found: {}", script);
+        return Future.succeededFuture();
+      }
+
+      String sqlScript = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
+      if (StringUtils.isBlank(sqlScript)) {
+        return Future.succeededFuture();
+      }
+
+      String tenantId = TenantTool.calculateTenantId(headers.get(X_OKAPI_TENANT));
+      String moduleName = PostgresClient.getModuleName();
+
+      sqlScript = sqlScript.replace(TENANT_PLACEHOLDER, tenantId).replace(MODULE_PLACEHOLDER, moduleName);
+
+      Promise<List<String>> promise = Promise.promise();
+      PostgresClient.getInstance(context.owner()).runSQLFile(sqlScript, false, promise);
+
+      LOGGER.info("Module is being deployed in test mode, test data will be initialized. Check the server log for details.");
+
+      return promise.future();
+    } catch (IOException e) {
+      return Future.failedFuture(e);
+    }
   }
 
 }
