4ac8d228315192fb06f50872cd3ec944a5b1e5f1
Andrii Novytskyi <60778653+andrii-novytskyi@users.noreply.github.com>
2020-06-11 15:13:58 +0300

    MDEXP-179 Modify job execution migration script (#96)

--- src/main/resources/templates/db_scripts/migration_scripts/rename_progress_current_to_exported.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/rename_progress_current_to_exported.sql
@@ -1,2 +1,3 @@
 UPDATE ${myuniversity}_${mymodule}.job_executions
-SET jsonb = jsonb_set(jsonb #- '{progress, current}', '{progress, exported}', jsonb -> 'progress' -> 'current');
+SET jsonb = jsonb_set(jsonb #- '{progress, current}', '{progress, exported}', jsonb -> 'progress' -> 'current')
+WHERE jsonb -> 'progress' -> 'current' IS NOT NULL;
