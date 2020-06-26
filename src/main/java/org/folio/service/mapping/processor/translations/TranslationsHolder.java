bc9ac003dd6a2cd4f2311983375ce3da1170ae2c
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-06-26 23:53:41 +0300

    MDEXP-101 Fix code smell

--- src/main/java/org/folio/service/mapping/processor/translations/TranslationsHolder.java
+++ src/main/java/org/folio/service/mapping/processor/translations/TranslationsHolder.java
@@ -13,7 +13,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.String.format;
