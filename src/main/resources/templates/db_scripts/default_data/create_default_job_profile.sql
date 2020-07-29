3cbbf2c13e8c0cc7e474a82f14fef98c11abd737
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-07-29 15:55:07 +0300

    MDEXP-247-Fix-Metadata Initial commit

--- src/main/resources/templates/db_scripts/default_data/create_default_job_profile.sql
+++ src/main/resources/templates/db_scripts/default_data/create_default_job_profile.sql
@@ -13,6 +13,14 @@ VALUES
          "lastName": "Process",
          "userName": "system_process"
        },
-       "mappingProfileId": "25d81cbe-9686-11ea-bb37-0242ac130002"
+       "mappingProfileId": "25d81cbe-9686-11ea-bb37-0242ac130002",
+       "metadata":{
+        "createdDate":"2020-07-28T00:00:00Z",
+        "createdByUserId":"00000000-0000-0000-0000-000000000000",
+        "createdByUsername":"system_process",
+        "updatedDate":"2020-07-28T00:00:00Z",
+        "updatedByUserId":"00000000-0000-0000-0000-000000000000",
+        "updatedByUsername":"system_process"
+      }
     }'
-  ) ON CONFLICT DO NOTHING;
+  ) ON CONFLICT(id) DO UPDATE SET jsonb = EXCLUDED.jsonb;
