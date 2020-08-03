ec88930350f3ce059ee3839c8802db4fdbff0f08
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-08-03 12:51:04 +0300

    MDEXP-216 Add comment to config

--- src/main/java/org/folio/service/transformationfields/TransformationFieldsConfig.java
+++ src/main/java/org/folio/service/transformationfields/TransformationFieldsConfig.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import static org.folio.service.mapping.referencedata.ReferenceDataImpl.IDENTIFIER_TYPES;
 
+/**
+ * Initial data for the transformation field. While extending the enum, put new values in alphabetical order
+ */
 public enum TransformationFieldsConfig {
 
   ID("id", "$.{recordType}.id"),
