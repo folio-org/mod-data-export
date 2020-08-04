aa0aebe503913528e14533d63acf83ea0e96274f
Kruthi vuppala <kvuppala@ebsco.com>
2020-08-04 18:51:10 -0400

    initial commit

--- src/main/java/org/folio/service/transformationfields/TransformationConfigConstants.java
+++ src/main/java/org/folio/service/transformationfields/TransformationConfigConstants.java
@@ -5,22 +5,22 @@ import java.util.Set;
 
 import static org.folio.service.transformationfields.TransformationFieldsConfig.*;
 
+
 public class TransformationConfigConstants {
 
   protected static final Set<TransformationFieldsConfig> INSTANCE_FIELDS_CONFIGS = EnumSet.of(
-    EDITIONS,
-    HR_ID,
-    IDENTIFIERS,
-    ID,
-    LANGUAGES,
-    SOURCE,
-    SUBJECTS,
-    LANGUAGES,
-    METADATA_CREATED_DATE,
-    METADATA_UPDATED_DATE,
-    METADATA_CREATED_BY_USER_ID,
-    METADATA_UPDATED_BY_USER_ID
-
+      EDITIONS,
+      HR_ID,
+      IDENTIFIERS,
+      ID,
+      LANGUAGES,
+      SOURCE,
+      SUBJECTS,
+      LANGUAGES,
+      METADATA_CREATED_DATE,
+      METADATA_UPDATED_DATE,
+      METADATA_CREATED_BY_USER_ID,
+      METADATA_UPDATED_BY_USER_ID
   );
 
   protected static final Set<TransformationFieldsConfig> HOLDINGS_FIELDS_CONFIGS = EnumSet.of(
