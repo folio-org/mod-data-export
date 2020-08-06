4f279c01eaec9bdc770f526ff3b78dd5c3b294b1
KVupp <41596468+KVupp@users.noreply.github.com>
2020-08-06 09:14:23 -0400

    MDEXP-210 and MDEXP-218- Simple fields for Instance and Item (#140)
    * Initial checkin
    
    * initial commit
    
    * Adding required fields
    
    * fixing unit test
    
    * formatting
    
    * fixing sysout

--- src/main/java/org/folio/service/transformationfields/TransformationConfigConstants.java
+++ src/main/java/org/folio/service/transformationfields/TransformationConfigConstants.java
@@ -1,22 +1,26 @@
 package org.folio.service.transformationfields;
 
+import static org.folio.service.transformationfields.TransformationFieldsConfig.*;
+
 import java.util.EnumSet;
 import java.util.Set;
 
-import static org.folio.service.transformationfields.TransformationFieldsConfig.HR_ID;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.ID;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.IDENTIFIERS;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.INSTANCE_ID;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.METADATA_CREATED_BY_USER_ID;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.METADATA_CREATED_DATE;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.METADATA_UPDATED_BY_USER_ID;
-import static org.folio.service.transformationfields.TransformationFieldsConfig.METADATA_UPDATED_DATE;
 
 public class TransformationConfigConstants {
 
   protected static final Set<TransformationFieldsConfig> INSTANCE_FIELDS_CONFIGS = EnumSet.of(
-    HR_ID,
-    IDENTIFIERS
+      EDITIONS,
+      HR_ID,
+      IDENTIFIERS,
+      ID,
+      LANGUAGES,
+      SOURCE,
+      SUBJECTS,
+      LANGUAGES,
+      METADATA_CREATED_DATE,
+      METADATA_UPDATED_DATE,
+      METADATA_CREATED_BY_USER_ID,
+      METADATA_UPDATED_BY_USER_ID
   );
 
   protected static final Set<TransformationFieldsConfig> HOLDINGS_FIELDS_CONFIGS = EnumSet.of(
@@ -30,7 +34,22 @@ public class TransformationConfigConstants {
   );
 
   protected static final Set<TransformationFieldsConfig> ITEM_FIELDS_CONFIGS = EnumSet.of(
-    HR_ID
+    HR_ID,
+    ID,
+    HOLDINGS_ID,
+    BARCODE,
+    VOLUME,
+    ENUMERATION,
+    CHRONOLOGY,
+    YEARCAPTION,
+    STATUS,
+    DESCRIPTION_OF_PIECES,
+    COPYNUMBER,
+    NUMBER_OF_PIECES,
+    METADATA_CREATED_DATE,
+    METADATA_UPDATED_DATE,
+    METADATA_CREATED_BY_USER_ID,
+    METADATA_UPDATED_BY_USER_ID
   );
 
   private TransformationConfigConstants() {
