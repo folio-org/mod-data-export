c039286d35b11a7d4e9842bdb3b025eda5a7fa44
Illia Borysenko <Illia_Borysenko@epam.com>
2020-10-09 17:02:52 +0300

    Fix MappingService unit test

--- src/test/java/org/folio/service/mapping/MappingServiceUnitTest.java
+++ src/test/java/org/folio/service/mapping/MappingServiceUnitTest.java
@@ -77,7 +77,6 @@ import static org.folio.TestUtil.readFileContentFromResources;
 import static org.folio.rest.jaxrs.model.RecordType.HOLDINGS;
 import static org.folio.rest.jaxrs.model.RecordType.INSTANCE;
 import static org.folio.rest.jaxrs.model.RecordType.ITEM;
-import static org.folio.service.mapping.referencedata.ReferenceDataImpl.MATERIAL_TYPES;
 import static org.folio.util.ExternalPathResolver.CALL_NUMBER_TYPES;
 import static org.folio.util.ExternalPathResolver.CAMPUSES;
 import static org.folio.util.ExternalPathResolver.CONTENT_TERMS;
@@ -90,11 +89,57 @@ import static org.folio.util.ExternalPathResolver.INSTITUTIONS;
 import static org.folio.util.ExternalPathResolver.LIBRARIES;
 import static org.folio.util.ExternalPathResolver.LOAN_TYPES;
 import static org.folio.util.ExternalPathResolver.LOCATIONS;
+import static org.folio.util.ExternalPathResolver.MATERIAL_TYPES;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import org.folio.TestUtil;
+import org.folio.clients.ConfigurationsClient;
+import org.folio.processor.ReferenceData;
+import org.folio.processor.rule.Rule;
+import org.folio.rest.jaxrs.model.ErrorLog;
+import org.folio.rest.jaxrs.model.MappingProfile;
+import org.folio.rest.jaxrs.model.RecordType;
+import org.folio.rest.jaxrs.model.TransformationField;
+import org.folio.rest.jaxrs.model.TransformationFieldCollection;
+import org.folio.rest.jaxrs.model.Transformations;
+import org.folio.service.logs.ErrorLogService;
+import org.folio.service.mapping.referencedata.ReferenceDataImpl;
+import org.folio.service.mapping.referencedata.ReferenceDataProvider;
+import org.folio.util.OkapiConnectionParams;
+import org.folio.util.ReferenceDataResponseUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.marc4j.marc.VariableField;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
