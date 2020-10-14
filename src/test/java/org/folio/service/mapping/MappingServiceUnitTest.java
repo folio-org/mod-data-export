cb8f3d38e23bece488c18e9bb70e472fc1b87ac2
igor_gorchakov <Igor_Gorchakov@epam.com>
2020-10-14 09:21:24 +0300

    fix unit tests

--- src/test/java/org/folio/service/mapping/MappingServiceUnitTest.java
+++ src/test/java/org/folio/service/mapping/MappingServiceUnitTest.java
@@ -25,12 +25,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.marc4j.marc.VariableField;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -95,51 +90,6 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-
-import com.google.common.collect.Lists;
-import com.google.common.io.Resources;
-import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonObject;
-import org.folio.TestUtil;
-import org.folio.clients.ConfigurationsClient;
-import org.folio.processor.ReferenceData;
-import org.folio.processor.rule.Rule;
-import org.folio.rest.jaxrs.model.ErrorLog;
-import org.folio.rest.jaxrs.model.MappingProfile;
-import org.folio.rest.jaxrs.model.RecordType;
-import org.folio.rest.jaxrs.model.TransformationField;
-import org.folio.rest.jaxrs.model.TransformationFieldCollection;
-import org.folio.rest.jaxrs.model.Transformations;
-import org.folio.service.logs.ErrorLogService;
-import org.folio.service.mapping.referencedata.ReferenceDataImpl;
-import org.folio.service.mapping.referencedata.ReferenceDataProvider;
-import org.folio.util.OkapiConnectionParams;
-import org.folio.util.ReferenceDataResponseUtil;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.marc4j.marc.VariableField;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-
-import org.junit.Assert;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
@@ -286,8 +236,8 @@ class MappingServiceUnitTest {
     List<VariableField> appendedMarcRecords = mappingService.mapFields(srsRecord, mappingProfile, jobExecutionId, params);
     // then
     //all transformations provided in the mapping profile must be mapped
-    Assert.assertEquals(42, appendedMarcRecords.stream().map(vf -> vf.getTag()).collect(Collectors.toSet()).size());
-    Assert.assertEquals(51, appendedMarcRecords.size());
+    Assert.assertEquals(41, appendedMarcRecords.stream().map(vf -> vf.getTag()).collect(Collectors.toSet()).size());
+    Assert.assertEquals(50, appendedMarcRecords.size());
   }
 
   @Test
@@ -420,7 +370,6 @@ class MappingServiceUnitTest {
     transformations.add(createTransformations("holdings.temporarylocation.campus.code", "$.holdings[*].temporaryLocationId", "921  $a", HOLDINGS));
     transformations.add(createTransformations("holdings.temporarylocation.institution.name", "$.holdings[*].temporaryLocationId", "922  $a", HOLDINGS));
     transformations.add(createTransformations("holdings.temporarylocation.institution.code", "$.holdings[*].temporaryLocationId", "923  $a", HOLDINGS));
-    transformations.add(createTransformations("holdings.callnumber", "$.holdings[*].callNumber", "990  $a", HOLDINGS));
     transformations.add(createTransformations("item.permanentlocation.name", "$.holdings[*].items[*].permanentLocationId", "924  $a", ITEM));
     transformations.add(createTransformations("item.permanentlocation.code", "$.holdings[*].items[*].permanentLocationId", "925  $a", ITEM));
     transformations.add(createTransformations("item.permanentlocation.library.name", "$.holdings[*].items[*].permanentLocationId", "926  $a", ITEM));
@@ -469,7 +418,7 @@ class MappingServiceUnitTest {
     List<Transformations> transformations = new ArrayList<>();
     AtomicInteger tag = new AtomicInteger(899);
     TransformationFieldCollection transformationFields = new JsonObject(
-        readFileContentFromResources("mapping/expectedTransformationFields.json")).mapTo(TransformationFieldCollection.class);
+      readFileContentFromResources("mapping/expectedTransformationFields.json")).mapTo(TransformationFieldCollection.class);
     transformationFields.getTransformationFields().stream()
       .filter(tfn -> tfn.getRecordType().equals(TransformationField.RecordType.INSTANCE))
       .forEach(tfn -> {
@@ -489,7 +438,7 @@ class MappingServiceUnitTest {
     List<Transformations> transformations = new ArrayList<>();
     AtomicInteger tag = new AtomicInteger(899);
     TransformationFieldCollection transformationFields = new JsonObject(
-        readFileContentFromResources("mapping/expectedTransformationFields.json")).mapTo(TransformationFieldCollection.class);
+      readFileContentFromResources("mapping/expectedTransformationFields.json")).mapTo(TransformationFieldCollection.class);
     transformationFields.getTransformationFields().stream()
       .filter(tfn -> tfn.getRecordType().equals(TransformationField.RecordType.HOLDINGS))
       .forEach(tfn -> {
