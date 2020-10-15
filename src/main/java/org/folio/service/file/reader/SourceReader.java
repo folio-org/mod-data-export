87107b6d405ee68e3f5babfe15fffca9185c5f2d
Andrii Novytskyi <60778653+andrii-novytskyi@users.noreply.github.com>
2020-10-15 19:02:41 +0300

    MDEXP-176 Handle csv files with other characters (#183)

--- src/main/java/org/folio/service/file/reader/SourceReader.java
+++ src/main/java/org/folio/service/file/reader/SourceReader.java
@@ -1,6 +1,7 @@
 package org.folio.service.file.reader;
 
 import org.folio.rest.jaxrs.model.FileDefinition;
+import org.folio.service.logs.ErrorLogService;
 
 import java.util.List;
 
@@ -12,10 +13,13 @@ public interface SourceReader {
   /**
    * Initialized source iterator configured by the given batch size
    *
-   * @param fileDefinition file definition
-   * @param batchSize      size batch for one stream iteration
+   * @param fileDefinition  file definition
+   * @param errorLogService service to log errors
+   * @param jobExecutionId  job execution id
+   * @param tenantId        tenant id
+   * @param batchSize       size batch for one stream iteration
    */
-  void init(FileDefinition fileDefinition, int batchSize);
+  void init(FileDefinition fileDefinition, ErrorLogService errorLogService, String jobExecutionId, String tenantId, int batchSize);
 
   /**
    * Returns {@code true} if the reader has more chunks to read.
