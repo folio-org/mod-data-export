ce1b54fcd17f79e4c440d3db9ef11fec1376295e
Illia Daliek <illia_daliek@epam.com>
2020-10-19 17:43:16 +0300

    add test

--- src/test/java/org/folio/rest/impl/DataExportTest.java
+++ src/test/java/org/folio/rest/impl/DataExportTest.java
@@ -120,6 +120,26 @@ class DataExportTest extends RestVerticleTestBase {
   }
 
   @Test
+  void testExport_uploadingCqlEmptyFile_FAILED_job(VertxTestContext context) throws IOException {
+    //given
+    String tenantId = okapiConnectionParams.getTenantId();
+    FileDefinition uploadedFileDefinition = uploadFile(EMPTY_FILE, CQL, buildRequestSpecification(tenantId));
+    // when
+    ExportRequest exportRequest = buildExportRequest(uploadedFileDefinition);
+    postRequest(JsonObject.mapFrom(exportRequest), EXPORT_URL);
+    String jobExecutionId = uploadedFileDefinition.getJobExecutionId();
+    // then
+    vertx.setTimer(TIMER_DELAY, handler ->
+      jobExecutionDao.getById(jobExecutionId, tenantId).onSuccess(optionalJobExecution -> {
+        JobExecution jobExecution = optionalJobExecution.get();
+        context.verify(() -> {
+          assertJobExecution(jobExecution, FAIL, EXPORTED_RECORDS_EMPTY);
+          context.completeNow();
+        });
+      }));
+  }
+
+  @Test
   void testExportByCSV_UnderlyingSrsOnly_COMPLETED_job(VertxTestContext context) throws IOException {
     // given
     String tenantId = okapiConnectionParams.getTenantId();
