20771a720cd723003d9526f30ae3f3ec3cfdc4bd
igor_gorchakov <Igor_Gorchakov@epam.com>
2020-10-20 15:12:04 +0300

    added check if progress is null

--- src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
@@ -1,2 +1,3 @@
 UPDATE ${myuniversity}_${mymodule}.job_executions
-SET jsonb = jsonb_set(jsonb #- '{progress, total}', '{progress, total}', to_jsonb((jsonb -> 'progress' ->> 'total')::int));
+SET jsonb = jsonb_set(jsonb #- '{progress, total}', '{progress, total}', to_jsonb((jsonb -> 'progress' ->> 'total')::int))
+WHERE jsonb -> 'progress' IS NOT NULL;
