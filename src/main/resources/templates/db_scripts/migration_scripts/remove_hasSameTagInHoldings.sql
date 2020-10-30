79198cdc7ded43bf3a524d4b7453e535faa93411
igor_gorchakov <Igor_Gorchakov@epam.com>
2020-10-30 17:52:14 +0200

    Fix code smells

--- src/main/resources/templates/db_scripts/migration_scripts/remove_hasSameTagInHoldings.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/remove_hasSameTagInHoldings.sql
@@ -2,7 +2,7 @@ UPDATE ${myuniversity}_${mymodule}.mapping_profiles
 SET jsonb = jsonb_set(jsonb, '{transformations}', subselect.clean_transformations)
 FROM
    (
-      SELECT jsonb_agg(transformations.value - 'hasSameTagInHoldings') as clean_transformations
+      SELECT jsonb_agg(transformations.value - 'hasSameTagInHoldings') AS clean_transformations
       FROM ${myuniversity}_${mymodule}.mapping_profiles, jsonb_array_elements(jsonb -> 'transformations') AS transformations
       GROUP BY jsonb
-    ) as subselect;
+    ) AS subselect;
