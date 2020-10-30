79a2ed3410c5e70cf4f51ddb0747b3ec362f76a8
igor_gorchakov <Igor_Gorchakov@epam.com>
2020-10-30 14:27:36 +0200

    Added migration script to remove 'hasSameTagInHoldings' field

--- src/main/resources/templates/db_scripts/migration_scripts/remove_hasSameTagInHoldings.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/remove_hasSameTagInHoldings.sql
@@ -1,1 +1,8 @@
-UPDATE ${myuniversity}_${mymodule}.job_executions SET jsonb = jsonb - 'hasSameTagInHoldings';
+UPDATE ${myuniversity}_${mymodule}.mapping_profiles
+SET jsonb = jsonb_set(jsonb, '{transformations}', subselect.clean_transformations)
+FROM
+   (
+      SELECT jsonb_agg(transformations.value - 'hasSameTagInHoldings') as clean_transformations
+      FROM ${myuniversity}_${mymodule}.mapping_profiles, jsonb_array_elements(jsonb -> 'transformations') AS transformations
+      GROUP BY jsonb
+    ) as subselect;
