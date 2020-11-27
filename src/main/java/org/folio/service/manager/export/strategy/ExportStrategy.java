def403365b34211a62007c8a3ae5960bda97bdcb
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-11-27 14:26:46 +0200

    MDEXP-285 Add java doc

--- src/main/java/org/folio/service/manager/export/strategy/ExportStrategy.java
+++ src/main/java/org/folio/service/manager/export/strategy/ExportStrategy.java
@@ -3,7 +3,16 @@ package org.folio.service.manager.export.strategy;
 import io.vertx.core.Promise;
 import org.folio.service.manager.export.ExportPayload;
 
+/**
+ * Export strategy interface, contain logic for export depends on record type
+ */
 public interface ExportStrategy {
 
+  /**
+   * Starts the export process for the chosen implementation: Instance, holdings or items
+   *
+   * @param exportPayload   {@ExportPayload}
+   * @param blockingPromise blocking promise
+   */
   void export(ExportPayload exportPayload, Promise<Object> blockingPromise);
 }
