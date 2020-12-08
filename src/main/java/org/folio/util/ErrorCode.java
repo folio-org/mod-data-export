75b61511999210e8940924ae014142762892438c
Illia Borysenko <Illia_Borysenko@epam.com>
2020-12-08 15:59:54 +0200

    Remove sapce for error log

--- src/main/java/org/folio/util/ErrorCode.java
+++ src/main/java/org/folio/util/ErrorCode.java
@@ -30,7 +30,7 @@ public enum ErrorCode {
   S3_BUCKET_IS_NOT_PROVIDED("error.s3.bucketIsNotProvided", "S3 bucket is not provided"),
   INVALID_UUID_FORMAT("error.invalidUuidFormat", "Invalid UUID format: %s"),
   DEFAULT_MAPPING_PROFILE_NOT_FOUND("error.mappingProfile.defaultNotFound", "Default mapping profile not found"),
-  ERROR_READING_FROM_INPUT_FILE("error.readingFromInputFile ", "Error while reading from input file with uuids or file is empty"),
+  ERROR_READING_FROM_INPUT_FILE("error.readingFromInputFile", "Error while reading from input file with uuids or file is empty"),
   SOME_UUIDS_NOT_FOUND("error.uuidsNotFound", "UUIDs not found in SRS or inventory: %s"),
   SOME_RECORDS_FAILED("error.someRecordsFailed", "Export is completed with errors, some records have failed to export, number of failed records: %s"),
   DATE_PARSE_ERROR_CODE("error.mapping.dateParse", "An error occurs during parsing the date while the mapping process"),
