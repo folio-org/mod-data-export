e51e8f4ac6b63e7d3ce89a4f11ffb00160af84ea
IlliaDaliek <illia_daliek@epam.com>
2021-07-08 14:38:15 +0300

    correct query

--- src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
@@ -2,11 +2,11 @@ UPDATE ${myuniversity}_${mymodule}.job_executions
 SET jsonb = jsonb_set(jsonb #- '{progress, total}', '{progress, total}', to_jsonb((jsonb -> 'progress' ->> 'total')::int))
 WHERE jsonb -> 'progress' IS NOT NULL AND jsonb -> 'progress' <> '{}';
 
-UPDATE diku_mod_data_export.job_executions
+UPDATE ${myuniversity}_${mymodule}.job_executions
 SET jsonb = jsonb_set(jsonb, '{progress}', '{"total": 0, "failed": 0, "exported": 0}'::jsonb)
 WHERE jsonb -> 'progress' IS NULL OR jsonb -> 'progress' = '{}';
 
-UPDATE diku_mod_data_export.job_executions
+UPDATE ${myuniversity}_${mymodule}.job_executions
 SET jsonb = jsonb_set(jsonb, '{progress}', jsonb_build_object('total', jsonb -> 'progress' -> 'exported','failed',0,'exported', jsonb -> 'progress' -> 'exported'))
 WHERE jsonb -> 'progress' IS NOT NULL AND jsonb -> 'progress' <> '{}'
 AND jsonb -> 'progress' -> 'exported' IS NOT NULL
