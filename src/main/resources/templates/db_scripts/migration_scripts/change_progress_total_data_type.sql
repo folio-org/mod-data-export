b2b372b8d70a69b1b7b64d7ef4e3acbb2fda934d
IlliaDaliek <illia_daliek@epam.com>
2021-07-08 12:31:55 +0300

    check

--- src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/change_progress_total_data_type.sql
@@ -5,3 +5,10 @@ WHERE jsonb -> 'progress' IS NOT NULL AND jsonb -> 'progress' <> '{}';
 UPDATE diku_mod_data_export.job_executions
 SET jsonb = jsonb_set(jsonb, '{progress}', '{"total": 0, "failed": 0, "exported": 0}'::jsonb)
 WHERE jsonb -> 'progress' IS NULL OR jsonb -> 'progress' = '{}';
+
+UPDATE diku_mod_data_export.job_executions
+SET jsonb = jsonb_set(jsonb, '{progress}', jsonb_build_object('total', jsonb -> 'progress' -> 'exported','failed',0,'exported', jsonb -> 'progress' -> 'exported'))
+WHERE jsonb -> 'progress' IS NOT NULL AND jsonb -> 'progress' <> '{}'
+AND jsonb -> 'progress' -> 'exported' IS NOT NULL
+AND jsonb -> 'progress' -> 'total' IS NULL
+AND jsonb -> 'progress' -> 'failed' IS NULL
