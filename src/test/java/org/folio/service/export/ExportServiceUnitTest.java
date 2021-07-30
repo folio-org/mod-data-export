c8774d448e139a65f0e05c497a120aa504bdf470
IlliaDaliek <illia_daliek@epam.com>
2021-07-30 14:06:32 +0300

    MDEXP-422: Gracefully handle MARC records that exceed the size limit
    (cherry picked from commit 3ed1f5652cc684e12c8fa8b908dc1f3c376a37f9)

--- src/test/java/org/folio/service/export/ExportServiceUnitTest.java
+++ src/test/java/org/folio/service/export/ExportServiceUnitTest.java
@@ -4,16 +4,21 @@ import io.vertx.core.json.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.util.Lists;
 import org.folio.TestUtil;
+import org.folio.clients.InventoryClient;
 import org.folio.rest.exceptions.ServiceException;
 import org.folio.rest.jaxrs.model.FileDefinition;
+import org.folio.rest.jaxrs.model.MappingProfile;
 import org.folio.service.export.storage.ExportStorageService;
 import org.folio.service.file.storage.FileStorage;
 import org.folio.service.logs.ErrorLogService;
+import org.folio.service.manager.export.ExportPayload;
 import org.folio.util.ErrorCode;
+import org.folio.util.OkapiConnectionParams;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.marc4j.MarcException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -23,9 +28,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static java.util.Collections.emptyList;
+import static org.folio.util.ErrorCode.ERROR_MARC_RECORD_CANNOT_BE_CONVERTED;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -36,6 +46,11 @@ class ExportServiceUnitTest {
   private static final String JOB_EXECUTION_ID = "jobExecutionId";
   private static final String RECORD_CONTENT = "record content";
   private static final String FILE_DEFINITION_ID = "file definition id";
+  private static final String TENANT = "tenant";
+  private static final String RECORDS_RESPONSE_JSON_FILE_PATH = "mockData/srs/get_records_response.json";
+  private static final String LONG_SRS_RECORD_JSON_FILE_PATH = "mockData/srs/srs_record_content_117000_characters_length.json";
+  private static final String INSTANCES_RESPONSE_JSON_FILE_PATH = "clients/inventory/get_instances_response.json";
+  private static final String INSTANCE_ID = "6666df22-5df3-412b-b9cc-cbdddb928e93";
 
   @Mock
   private FileStorage fileStorage;
@@ -43,20 +58,25 @@ class ExportServiceUnitTest {
   private ExportStorageService exportStorageService;
   @Mock
   private ErrorLogService errorLogService;
+  @Mock
+  private InventoryClient inventoryClient;
   @InjectMocks
   private ExportService exportService = new LocalFileSystemExportService();
 
-  private static final String TENANT = "tenant";
+  private final FileDefinition fileDefinition = new FileDefinition();
+  private final String jobExecutionId = UUID.randomUUID().toString();
+  private final OkapiConnectionParams params = new OkapiConnectionParams();
+  private final ExportPayload exportPayload = new ExportPayload(emptyList(), true, fileDefinition, params, jobExecutionId, new MappingProfile());
 
   @Test
   void shouldPassExportFor_1_SrsRecord() {
     // given
-    String response = TestUtil.readFileContentFromResources("mockData/srs/get_records_response.json");
+    String response = TestUtil.readFileContentFromResources(RECORDS_RESPONSE_JSON_FILE_PATH);
     String jsonRecord = new JsonObject(response).getJsonArray("sourceRecords").getJsonObject(0).toString();
-    FileDefinition fileDefinition = new FileDefinition();
+
     when(fileStorage.saveFileDataBlocking(any(byte[].class), any(FileDefinition.class))).thenReturn(fileDefinition);
     // when
-    exportService.exportSrsRecord(Collections.singletonList(jsonRecord), fileDefinition);
+    exportService.exportSrsRecord(Collections.singletonList(jsonRecord), exportPayload);
     // then
     Mockito.verify(fileStorage, Mockito.times(1)).saveFileDataBlocking(any(byte[].class), any(FileDefinition.class));
   }
@@ -65,11 +85,25 @@ class ExportServiceUnitTest {
   void shouldPassExportFor_NULL_SrsRecords() {
     // given
     List<String> marcRecords = null;
-    FileDefinition fileDefinition = new FileDefinition();
     // when
-    exportService.exportSrsRecord(marcRecords, fileDefinition);
+    exportService.exportSrsRecord(marcRecords, exportPayload);
     // then
     Mockito.verify(fileStorage, Mockito.times(0)).saveFileDataBlocking(any(byte[].class), any(FileDefinition.class));
+  }
+
+  @Test
+  void shouldSaveErrorLog_whenJsonCannotBeConvertedToMarcRecordBecauseOfLargeSize() {
+    //given
+    String record = TestUtil.readFileContentFromResources(LONG_SRS_RECORD_JSON_FILE_PATH);
+    String stringJson = TestUtil.readFileContentFromResources(INSTANCES_RESPONSE_JSON_FILE_PATH);
+    JsonObject instances = new JsonObject(stringJson);
+    JsonObject instance = instances.getJsonArray("instances").getJsonObject(0);
+
+    when(inventoryClient.getInstancesByIds(Collections.singletonList(INSTANCE_ID), jobExecutionId, params, 1)).thenReturn(Optional.of(instances));
+    //when
+    exportService.exportSrsRecord(Collections.singletonList(record), exportPayload);
+    //then
+    Mockito.verify(errorLogService, Mockito.times(1)).saveWithAffectedRecord(eq(instance), eq(ERROR_MARC_RECORD_CANNOT_BE_CONVERTED.getCode()), eq(jobExecutionId), any(MarcException.class), eq(params));
   }
 
   @Test
@@ -99,9 +133,8 @@ class ExportServiceUnitTest {
   void shouldNotStoreMarcInFile_whenSRSRecordIsEmpty() {
     // given
     String inventoryRecord = StringUtils.EMPTY;
-    FileDefinition fileDefinition = new FileDefinition();
     // when
-    exportService.exportSrsRecord(Collections.singletonList(inventoryRecord), fileDefinition);
+    exportService.exportSrsRecord(Collections.singletonList(inventoryRecord), exportPayload);
     // then
     Mockito.verify(fileStorage, never()).saveFileDataBlocking(any(byte[].class), any(FileDefinition.class));
   }
