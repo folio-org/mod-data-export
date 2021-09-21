e64d8664a449ad1900bf2c22c616a5a5064e97b5
Oleksandr Bozhko <oleksandr_bozhko@epam.com>
2021-09-21 15:07:49 +0300

    MDEXP-426 Added test case

--- src/test/java/org/folio/service/export/ExportServiceUnitTest.java
+++ src/test/java/org/folio/service/export/ExportServiceUnitTest.java
@@ -209,6 +209,25 @@ class ExportServiceUnitTest {
   }
 
   @Test
+  void shouldSaveErrorLog_whenJsonCannotBeConvertedToMarcRecordBecauseOfInvalidInstanceField_999() {
+    //given
+    String record = "{\"leader\":\"00000nam a2200000 a 4500\",\"fields\":[" +
+      "{\"001\":\"in00000000011\"}," +
+      "{\"005\":\"20210728150129.6\"}," +
+      "{\"008\":\"950721s1996    nyua     b    000 0 eng  \"}," +
+      "{\"945\":{\"subfields\":[{\"z\":\"05-22-15\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+      "{999\":{\"subfields\":[{\"s\":\"b26859f1-8c79-47b3-b047-1ecde668492f\"}," + // Invalid instance field: no opening quotes.
+      "{\"i\":\"6666df22-5df3-412b-b9cc-cbdddb928e93\"}],\"ind1\":\"f\",\"ind2\":\"f\"}}]}\n";
+    JsonObject instance = new JsonObject(); // In this case instance is just empty object because 999 field is corrupted.
+    Pair<List<String>, Integer> marcRecordsToExport = MutablePair.of(Collections.singletonList(record), 0);
+    //when
+    exportService.exportSrsRecord(marcRecordsToExport, exportPayload);
+    //then
+    Mockito.verify(errorLogService, Mockito.times(1)).saveWithAffectedRecord(eq(instance), eq(ERROR_MARC_RECORD_CONTAINS_CONTROL_CHARACTERS.getCode()), eq(jobExecutionId), any(MarcException.class), eq(params));
+    assertEquals(1, marcRecordsToExport.getValue().intValue());
+  }
+
+  @Test
   void shouldPassExportFor_1_InventoryRecord() {
     // given
     String inventoryRecord = "testRecord";
