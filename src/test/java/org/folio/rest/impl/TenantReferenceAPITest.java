b43be518637759e26324d89db5c352ede2ed47c4
Alherd_Nestsiarkow <s1mple@tut.by>
2021-10-22 16:41:04 +0300

    Increase coverage Rename default job profile

--- src/test/java/org/folio/rest/impl/TenantReferenceAPITest.java
+++ src/test/java/org/folio/rest/impl/TenantReferenceAPITest.java
@@ -1,6 +1,7 @@
 package org.folio.rest.impl;
 
 import io.vertx.core.Context;
+import org.folio.rest.jaxrs.model.TenantAttributes;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -21,6 +22,16 @@ class TenantReferenceAPITest extends RestVerticleTestBase {
     tenantReferenceAPI.deleteTenantByOperationId(operationId, headers, handler -> {
       assertTrue(handler.succeeded());
     },ctx);
+  }
+
+  @Test
+  void loadInvalidReferenceDataTest() {
+    TenantReferenceAPI tenantReferenceAPI = new TenantReferenceAPI();
+    TenantAttributes attributes = new TenantAttributes();
+    Map<String, String> headers = new HashMap<>();
+    Context vertxContext = vertx.getOrCreateContext();
+
+    assertTrue(tenantReferenceAPI.loadData(attributes, TENANT_ID, headers, vertxContext).failed());
   }
 
 }
