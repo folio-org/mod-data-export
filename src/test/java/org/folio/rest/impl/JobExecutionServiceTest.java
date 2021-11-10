304ac1ba31615971df496221a45dc60503f4192f
Siarhei Charniak <siarhei_charniak@epam.com>
2021-11-10 19:24:44 +0300

    MDEXP-470 - Export fails after /data-export/expire-jobs endpoint invocation

--- src/test/java/org/folio/rest/impl/JobExecutionServiceTest.java
+++ src/test/java/org/folio/rest/impl/JobExecutionServiceTest.java
@@ -6,12 +6,15 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.apache.http.HttpStatus;
-import org.folio.config.ApplicationConfig;
+import org.folio.rest.jaxrs.model.ErrorLog;
 import org.folio.rest.jaxrs.model.JobExecution;
 import org.folio.rest.jaxrs.model.JobExecution.Status;
+import org.folio.rest.jaxrs.model.Progress;
 import org.folio.service.export.storage.ExportStorageService;
 import org.folio.service.job.JobExecutionService;
+import org.folio.service.logs.ErrorLogService;
 import org.folio.spring.SpringContextUtil;
+import org.folio.util.ErrorCode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,14 +23,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.UUID;
 
@@ -45,6 +45,8 @@ class JobExecutionServiceTest extends RestVerticleTestBase {
 
   @Autowired
   private JobExecutionService jobExecutionService;
+  @Autowired
+  private ErrorLogService errorLogService;
 
   public JobExecutionServiceTest() {
     Context vertxContext = vertx.getOrCreateContext();
@@ -84,12 +86,26 @@ class JobExecutionServiceTest extends RestVerticleTestBase {
     String dateString = "2020-09-15T11:02:00.847+0000";
     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSSZ");
     Date date = dateFormat.parse(dateString);
+    Progress progress = new Progress().withExported(7).withTotal(777).withFailed(77);
+    String errorLogId = UUID.randomUUID().toString();
+
     JobExecution jobExecution = new JobExecution()
+      .withHrId(7777)
       .withJobProfileId("6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a")
       .withStatus(Status.IN_PROGRESS)
-      .withLastUpdatedDate(date);
+      .withLastUpdatedDate(date)
+      .withProgress(progress);
 
-    jobExecutionService.save(jobExecution, TENANT_ID);
+    ErrorLog errorLog = new ErrorLog()
+      .withErrorMessageCode(ErrorCode.SOME_RECORDS_FAILED.getCode())
+      .withId(errorLogId)
+      .withLogLevel(ErrorLog.LogLevel.ERROR)
+      .withErrorMessageValues(Collections.emptyList());
+
+    jobExecutionService.save(jobExecution, TENANT_ID).onSuccess(jobExecutionAsyncResult -> {
+      errorLog.setJobExecutionId(jobExecutionAsyncResult.getId());
+      errorLogService.save(errorLog, TENANT_ID);
+    });
 
     vertx.setTimer(3000L, handler -> {
 
