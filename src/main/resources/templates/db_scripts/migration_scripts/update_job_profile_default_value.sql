8bc42c5b33750adab561006d37b88b4043b75230
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2022-02-24 11:02:57 +0300

    MDEXP-213 - update job profile

---
+++ src/main/resources/templates/db_scripts/migration_scripts/update_job_profile_default_value.sql
@@ -0,0 +1,7 @@
+UPDATE ${myuniversity}_${mymodule}.job_profiles
+SET jsonb = jsonb_set(jsonb, '{default}', 'true')
+WHERE id = '5e9835fc-0e51-44c8-8a47-f7b8fce35da7' OR id = '6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a';
+
+UPDATE ${myuniversity}_${mymodule}.job_profiles
+SET jsonb = jsonb_set(jsonb, '{default}', 'false')
+WHERE id != '5e9835fc-0e51-44c8-8a47-f7b8fce35da7' OR id != '6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a';
