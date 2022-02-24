38c8f5f0859a3b7b95df58b0c7300b818d49767e
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2022-02-24 12:16:39 +0300

    MDEXP-213 - update migrations scripts

--- src/main/resources/templates/db_scripts/migration_scripts/update_job_profile_default_value.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/update_job_profile_default_value.sql
@@ -4,4 +4,4 @@ WHERE id = '5e9835fc-0e51-44c8-8a47-f7b8fce35da7' OR id = '6f7f3cd7-9f24-42eb-ae
 
 UPDATE ${myuniversity}_${mymodule}.job_profiles
 SET jsonb = jsonb_set(jsonb, '{default}', 'false')
-WHERE id != '5e9835fc-0e51-44c8-8a47-f7b8fce35da7' OR id != '6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a';
+WHERE id != '5e9835fc-0e51-44c8-8a47-f7b8fce35da7' AND id != '6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a';
--- src/main/resources/templates/db_scripts/migration_scripts/update_mapping_profile_default_value.sql
+++ src/main/resources/templates/db_scripts/migration_scripts/update_mapping_profile_default_value.sql
@@ -4,4 +4,4 @@ WHERE id = '1ef7d0ac-f0a8-42b5-bbbb-c7e249009c13' OR id = '25d81cbe-9686-11ea-bb
 
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
 SET jsonb = jsonb_set(jsonb, '{default}', 'false')
-WHERE id != '1ef7d0ac-f0a8-42b5-bbbb-c7e249009c13' OR id != '25d81cbe-9686-11ea-bb37-0242ac130002';
+WHERE id != '1ef7d0ac-f0a8-42b5-bbbb-c7e249009c13' AND id != '25d81cbe-9686-11ea-bb37-0242ac130002';
