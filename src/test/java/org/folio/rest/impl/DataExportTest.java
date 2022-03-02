9506f5c1f8eed8c44847b82528b61c6005fa377f
Oleksandr_Bozhko <Oleksandr_Bozhko@epam.com>
2022-03-02 13:18:35 +0200

    MDEXP-477 Fix test

--- src/test/java/org/folio/rest/impl/DataExportTest.java
+++ src/test/java/org/folio/rest/impl/DataExportTest.java
@@ -79,7 +79,7 @@ import static org.mockito.Mockito.doNothing;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DataExportTest extends RestVerticleTestBase {
 
-  private static final long TIMER_DELAY = 5000L;
+  private static final long TIMER_DELAY = 12000L;
   private static final String INSTANCE_UUIDS_FOR_COMPLETED_JOB = "uuids_for_completed_job.csv";
   private static final String INSTANCE_UUIDS_FOR_COMPLETED_WITH_ERRORS_JOB = "uuids_for_completed_with_errors_job.csv";
   private static final String INSTANCE_UUIDS_INVENTORY = "instance_uuids_inventory.csv";
@@ -140,7 +140,7 @@ class DataExportTest extends RestVerticleTestBase {
     context.awaitCompletion(5, TimeUnit.SECONDS);
     String jobExecutionId = uploadedFileDefinition.getJobExecutionId();
     // then
-    vertx.setTimer(7000L, handler ->
+    vertx.setTimer(TIMER_DELAY, handler ->
       jobExecutionDao.getById(jobExecutionId, tenantId).onSuccess(optionalJobExecution -> {
         JobExecution jobExecution = optionalJobExecution.get();
         context.verify(() -> {
