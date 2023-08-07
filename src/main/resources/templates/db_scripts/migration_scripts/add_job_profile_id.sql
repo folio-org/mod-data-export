4cb68adf16d2d6ede7e5dd37cb7e49f405306314
siarhei-charniak <siarhei_charniak@epam.com>
2023-08-07 13:44:50 +0300

    MDEXP-622 - Create a project skeleton in a new branch (#361)

--- src/main/resources/templates/db_scripts/migration_scripts/add_job_profile_id.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ${myuniversity}_${mymodule}.job_executions
-SET jsonb = jsonb_set(jsonb , '{jobProfileId}', '"6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a"')
-WHERE jsonb -> 'jobProfileId' IS NULL;
--- src/main/resources/templates/db_scripts/migration_scripts/update_mapping_profile_default_value.sql
+++
@@ -1,7 +0,0 @@
-UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_set(jsonb, '{default}', 'true')
-WHERE id = '1ef7d0ac-f0a8-42b5-bbbb-c7e249009c13' OR id = '25d81cbe-9686-11ea-bb37-0242ac130002';
-
-UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_set(jsonb, '{default}', 'false')
-WHERE id != '1ef7d0ac-f0a8-42b5-bbbb-c7e249009c13' AND id != '25d81cbe-9686-11ea-bb37-0242ac130002';
