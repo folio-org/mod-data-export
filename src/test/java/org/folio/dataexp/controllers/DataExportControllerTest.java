f40a32c08b5cd344f544d06e5a7ced46466ab05b
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-08-17 10:59:50 +0300

    MDEXP-629 - controller tests

--- src/test/java/org/folio/dataexp/controllers/DataExportControllerTest.java
+++ src/test/java/org/folio/dataexp/controllers/DataExportControllerTest.java
@@ -5,6 +5,7 @@ import org.folio.dataexp.BaseTest;
 import org.folio.dataexp.domain.dto.FileDefinition;
 import org.folio.dataexp.domain.entity.FileDefinitionEntity;
 import org.folio.dataexp.repository.FileDefinitionEntityRepository;
+import org.folio.dataexp.service.DataExportService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +22,8 @@ public class DataExportControllerTest extends BaseTest {
 
   @MockBean
   private FileDefinitionEntityRepository fileDefinitionEntityRepository;
+  @MockBean
+  private DataExportService dataExportService;
 
   @Test
   @SneakyThrows
@@ -28,13 +32,15 @@ public class DataExportControllerTest extends BaseTest {
     fileDefinition.setFileName("test.csv");
     fileDefinition.setId(UUID.randomUUID());
     fileDefinition.setUploadFormat(FileDefinition.UploadFormatEnum.CSV);
+
+    when(dataExportService.postFileDefinition(isA(FileDefinition.class))).thenReturn(fileDefinition);
+
     mockMvc.perform(MockMvcRequestBuilders
         .post("/data-export/file-definitions")
         .headers(defaultHeaders())
         .contentType(APPLICATION_JSON)
         .content(asJsonString(fileDefinition)))
       .andExpect(status().isCreated());
-    fileDefinitionEntityRepository.getReferenceById(fileDefinition.getId());
   }
 
   @Test
