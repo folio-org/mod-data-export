d440c687f867467547e7f5667f66424852714c03
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-08-18 15:59:27 +0300

    MDEXP-629 - update test

---
+++ src/test/java/org/folio/dataexp/service/export/storage/FolioS3ClientFactoryTest.java
@@ -0,0 +1,20 @@
+package org.folio.dataexp.service.export.storage;
+
+import org.folio.dataexp.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class FolioS3ClientFactoryTest extends BaseTest {
+
+  @Autowired
+  private FolioS3ClientFactory folioS3ClientFactory;
+
+  @Test
+  void getFolioS3ClientTest() {
+    var s3Client1= folioS3ClientFactory.getFolioS3Client();
+    var s3Client2= folioS3ClientFactory.getFolioS3Client();
+    assertSame(s3Client1, s3Client2);
+  }
+}
