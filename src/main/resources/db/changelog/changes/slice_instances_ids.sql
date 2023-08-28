25770ebf44d8d77e051ec0b94ff90e7566513a4b
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-08-28 10:32:12 +0300

    MDEXP-629 - rename init procedures sql

--- src/main/resources/db/changelog/changes/slice_instances_ids.sql
+++ src/main/resources/db/changelog/changes/slice_instances_ids.sql
@@ -1,14 +1,3 @@
-CREATE OR REPLACE PROCEDURE save_instances_ids(jobExecutionId text, sqlWhere text)
-LANGUAGE plpgsql
-AS $$
-DECLARE
-  executeSql text;
-BEGIN
- executeSql = 'insert into job_executions_export_ids(job_execution_id, instance_id) select ''' || jobExecutionId || ''' as job_execution_id, id as instance_id from v_instance ' || sqlWhere;
-EXECUTE executeSql;
-END;
-$$;
-
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE OR REPLACE PROCEDURE slice_instances_ids(jobExecutionId text, fileLocation text, sliceSize int)
 LANGUAGE plpgsql
