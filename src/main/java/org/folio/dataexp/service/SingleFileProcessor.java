c10e1339dc72d2c85cfc192229985db61c87891c
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-09-05 10:37:42 +0300

    MDEXP-630 - update job execution status if empty to export

--- src/main/java/org/folio/dataexp/service/SingleFileProcessor.java
+++ src/main/java/org/folio/dataexp/service/SingleFileProcessor.java
@@ -3,12 +3,15 @@ package org.folio.dataexp.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.dataexp.domain.dto.ExportRequest;
+import org.folio.dataexp.domain.dto.JobExecution;
 import org.folio.dataexp.domain.entity.JobExecutionExportFilesEntity;
+import org.folio.dataexp.repository.JobExecutionEntityRepository;
 import org.folio.dataexp.repository.JobExecutionExportFilesEntityRepository;
 import org.folio.dataexp.service.export.ExportExecutor;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.Date;
 import java.util.UUID;
 
 @Component
@@ -18,10 +21,18 @@ public class SingleFileProcessor {
 
   protected final ExportExecutor exportExecutor;
   private final JobExecutionExportFilesEntityRepository jobExecutionExportFilesEntityRepository;
+  private final JobExecutionEntityRepository jobExecutionEntityRepository;
 
   public void exportBySingleFile(UUID jobExecutionId, ExportRequest.RecordTypeEnum recordType) {
     var exports = jobExecutionExportFilesEntityRepository.findByJobExecutionId(jobExecutionId);
-    if (exports.size() == 0) return;
+    if (exports.isEmpty()) {
+      log.warn("Nothing to export for job execution {}", jobExecutionId);
+      var jobExecutionEntity = jobExecutionEntityRepository.getReferenceById(jobExecutionId);
+      var jobExecution = jobExecutionEntity.getJobExecution();
+      jobExecution.setLastUpdatedDate(new Date());
+      jobExecution.setStatus(JobExecution.StatusEnum.FAIL);
+      return;
+    }
     var file = new File(exports.get(0).getFileLocation());
     var parent = file.getParentFile();
     if (!parent.exists()) {
--- src/main/java/org/folio/dataexp/service/SingleFileProcessorAsync.java
+++ src/main/java/org/folio/dataexp/service/SingleFileProcessorAsync.java
@@ -2,6 +2,7 @@ package org.folio.dataexp.service;
 
 import org.folio.dataexp.domain.dto.ExportRequest;
 import org.folio.dataexp.domain.entity.JobExecutionExportFilesEntity;
+import org.folio.dataexp.repository.JobExecutionEntityRepository;
 import org.folio.dataexp.repository.JobExecutionExportFilesEntityRepository;
 import org.folio.dataexp.service.export.ExportExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class SingleFileProcessorAsync extends SingleFileProcessor {
 
+
   @Autowired
-  public SingleFileProcessorAsync(ExportExecutor exportExecutor, JobExecutionExportFilesEntityRepository jobExecutionExportFilesEntityRepository) {
-    super(exportExecutor, jobExecutionExportFilesEntityRepository);
+  public SingleFileProcessorAsync(ExportExecutor exportExecutor, JobExecutionExportFilesEntityRepository jobExecutionExportFilesEntityRepository, JobExecutionEntityRepository jobExecutionEntityRepository) {
+    super(exportExecutor, jobExecutionExportFilesEntityRepository, jobExecutionEntityRepository);
   }
 
   @Override
