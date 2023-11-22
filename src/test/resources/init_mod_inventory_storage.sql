86e33b5d97db8c52f1080bb24a38436a69321212
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-11-22 10:45:29 +0300

    MDEXP-628 - fix tests

--- src/test/resources/init_mod_inventory_storage.sql
+++ src/test/resources/init_mod_inventory_storage.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA diku_mod_inventory_storage;
 CREATE TABLE diku_mod_inventory_storage.instance (id UUID, jsonb JSONB);
-CREATE TABLE diku_mod_inventory_storage.holdings_record (id UUID);
-CREATE TABLE diku_mod_inventory_storage.item (id UUID);
+CREATE TABLE diku_mod_inventory_storage.holdings_record (id UUID, jsonb JSONB, instanceid UUID);
+CREATE TABLE diku_mod_inventory_storage.item (id UUID, jsonb JSONB, holdingsrecordid UUID);
 CREATE TABLE diku_mod_inventory_storage.authority (id UUID);
 
 INSERT INTO diku_mod_inventory_storage.instance (id, jsonb) VALUES ('011e1aea-222d-4d1d-957d-0abcdd0e9acd',
