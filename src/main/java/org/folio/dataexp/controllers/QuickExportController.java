db7a18dc40a749c851a1140eddf051c80563798a
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-02-15 15:52:42 +0200

    MDEXP-641 Initial commit

---
+++ src/main/java/org/folio/dataexp/controllers/QuickExportController.java
@@ -0,0 +1,27 @@
+package org.folio.dataexp.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.dataexp.domain.dto.QuickExportRequest;
+import org.folio.dataexp.domain.dto.QuickExportResponse;
+import org.folio.dataexp.rest.resource.QuickExportApi;
+import org.folio.dataexp.service.QuickExportService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/data-export")
+public class QuickExportController implements QuickExportApi {
+
+  private final QuickExportService quickExportService;
+
+  @Override
+  public ResponseEntity<QuickExportResponse> postDataExportQuickExport(QuickExportRequest quickExportRequest) {
+    quickExportService.postQuickExport(quickExportRequest);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
---
+++ src/main/java/org/folio/dataexp/service/QuickExportService.java
@@ -0,0 +1,66 @@
+package org.folio.dataexp.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.dataexp.domain.dto.ExportRequest;
+import org.folio.dataexp.domain.dto.FileDefinition;
+import org.folio.dataexp.domain.dto.QuickExportRequest;
+import org.folio.dataexp.domain.entity.ExportIdEntity;
+import org.folio.dataexp.exception.export.DataExportRequestValidationException;
+import org.folio.dataexp.repository.ExportIdEntityRepository;
+import org.folio.dataexp.repository.JobProfileEntityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class QuickExportService {
+
+  private final FileDefinitionsService fileDefinitionsService;
+  private final DataExportService dataExportService;
+  private final JobProfileEntityRepository jobProfileEntityRepository;
+  private final ExportIdEntityRepository exportIdEntityRepository;
+
+  public void postQuickExport(QuickExportRequest quickExportRequest) {
+    var fileDefinition = new FileDefinition().id(UUID.randomUUID()).size(0).fileName(quickExportRequest.getType() + "-quick.csv");
+    fileDefinitionsService.postFileDefinition(fileDefinition);
+    log.info("Post quick export for job profile {}", quickExportRequest.getJobProfileId());
+    dataExportService.postDataExport(getExportRequestFromQuickExportRequest(quickExportRequest, fileDefinition));
+  }
+
+  private ExportRequest getExportRequestFromQuickExportRequest(QuickExportRequest quickExportRequest, FileDefinition fileDefinition) {
+    var exportRequest = new ExportRequest();
+    saveBatch(quickExportRequest, fileDefinition);
+    exportRequest.setJobProfileId(getDefaultJobProfileId(quickExportRequest));
+    exportRequest.setRecordType(ExportRequest.RecordTypeEnum.fromValue(quickExportRequest.getRecordType().getValue()));
+    exportRequest.setFileDefinitionId(fileDefinition.getId());
+    exportRequest.setQuick(true);
+    return exportRequest;
+  }
+
+  private void saveBatch(QuickExportRequest quickExportRequest, FileDefinition fileDefinition) {
+    var batch = new ArrayList<ExportIdEntity>();
+    quickExportRequest.getUuids().forEach(instanceId -> {
+      var entity = ExportIdEntity.builder().jobExecutionId(fileDefinition
+        .getJobExecutionId()).instanceId(instanceId).build();
+      batch.add(entity);
+    });
+    exportIdEntityRepository.saveAll(batch);
+  }
+
+  private UUID getDefaultJobProfileId(QuickExportRequest quickExportRequest) {
+    var ids = jobProfileEntityRepository.findIdOfDefaultJobProfileByName(quickExportRequest.getRecordType().getValue().toLowerCase());
+    if (ids.isEmpty()) {
+      log.error("No default job profile found by the following recordType: {}", quickExportRequest.getRecordType());
+      throw new DataExportRequestValidationException("No default job profile found by the following recordType: " + quickExportRequest.getRecordType());
+    }
+    if (ids.size() > 1) {
+      log.warn("More than 1 job profile found by the following recordType: {}, only first one will be used: {}",
+        quickExportRequest.getRecordType(), ids.get(0));
+    }
+    return ids.get(0);
+  }
+}
