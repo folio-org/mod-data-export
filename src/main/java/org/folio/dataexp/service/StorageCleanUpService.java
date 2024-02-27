2c71c1c4d9bdd61a8513353ab4505b5296068528
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2024-02-27 17:08:51 +0300

    MDEXP-720 - Clean job_executions_export_ids table after export for spring base module (#436)
    * MDEXP-720 - clean ids
    
    * MDEXP-720 - update tests

--- src/main/java/org/folio/dataexp/service/StorageCleanUpService.java
+++ src/main/java/org/folio/dataexp/service/StorageCleanUpService.java
@@ -4,12 +4,15 @@ import static java.util.concurrent.TimeUnit.HOURS;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.folio.dataexp.repository.ExportIdEntityRepository;
 import org.folio.dataexp.repository.FileDefinitionEntityRepository;
 import org.folio.dataexp.util.S3FilePathUtils;
 import org.folio.s3.client.FolioS3Client;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ import java.util.Date;
 public class StorageCleanUpService {
   private final FileDefinitionEntityRepository fileDefinitionEntityRepository;
   private final FolioS3Client storageClient;
+  private final ExportIdEntityRepository exportIdEntityRepository;
 
   public void cleanExpiredFilesAndFileDefinitions() {
     var expirationDate = new Date(new Date().getTime() - HOURS.toMillis(1));
@@ -27,5 +31,10 @@ public class StorageCleanUpService {
       storageClient.remove(S3FilePathUtils.getPathToUploadedFiles(fileDefinition.getId(), fileDefinition.getFileName()));
       fileDefinitionEntityRepository.delete(fileDefinitionEntity);
     });
+  }
+
+  @Transactional
+  public void cleanExportIdEntities(UUID jobExecutionId) {
+    exportIdEntityRepository.deleteByJobExecutionId(jobExecutionId);
   }
 }
--- src/test/java/org/folio/dataexp/service/export/ExportExecutorTest.java
+++ src/test/java/org/folio/dataexp/service/export/ExportExecutorTest.java
@@ -14,6 +14,7 @@ import org.folio.dataexp.repository.FileDefinitionEntityRepository;
 import org.folio.dataexp.repository.JobExecutionEntityRepository;
 import org.folio.dataexp.repository.JobExecutionExportFilesEntityRepository;
 import org.folio.dataexp.service.CommonExportFails;
+import org.folio.dataexp.service.StorageCleanUpService;
 import org.folio.dataexp.service.export.strategies.ExportStrategyStatistic;
 import org.folio.dataexp.service.export.strategies.InstancesExportStrategy;
 import org.folio.dataexp.service.logs.ErrorLogService;
@@ -49,6 +50,8 @@ class ExportExecutorTest {
   private FileDefinitionEntityRepository fileDefinitionEntityRepository;
   @Mock
   private S3ExportsUploader s3ExportsUploader;
+  @Mock
+  private StorageCleanUpService storageCleanUpService;
 
   @InjectMocks
   private ExportExecutor exportExecutor;
@@ -93,6 +96,7 @@ class ExportExecutorTest {
     assertEquals(JobExecutionExportFilesStatus.ACTIVE, exportEntity.getStatus());
     assertEquals(JobExecution.StatusEnum.COMPLETED, jobExecution.getStatus());
     verify(s3ExportsUploader).upload(jobExecution, List.of(completedExportEntity), "file_name");
+    verify(storageCleanUpService).cleanExportIdEntities(jobExecution.getId());
   }
 
   @Test
@@ -137,5 +141,6 @@ class ExportExecutorTest {
     assertEquals(JobExecution.StatusEnum.COMPLETED_WITH_ERRORS, jobExecution.getStatus());
     verify(errorLogService).saveCommonExportFailsErrors(commonFails, 2, jobExecutionId);
     verify(s3ExportsUploader).upload(jobExecution, List.of(completedExportEntity), "file_name");
+    verify(storageCleanUpService).cleanExportIdEntities(jobExecution.getId());
   }
 }
