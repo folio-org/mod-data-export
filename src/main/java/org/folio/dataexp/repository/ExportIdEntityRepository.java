2fd87182f9d82764453cb824c7aa1af48797dd6d
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-02-28 16:48:41 +0300

    MDEXP-720 - delete by query

--- src/main/java/org/folio/dataexp/repository/ExportIdEntityRepository.java
+++ src/main/java/org/folio/dataexp/repository/ExportIdEntityRepository.java
@@ -4,8 +4,10 @@ import org.folio.dataexp.domain.entity.ExportIdEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.rmi.server.UID;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -21,7 +23,9 @@ public interface ExportIdEntityRepository extends JpaRepository<ExportIdEntity, 
 
   long countByJobExecutionIdIsAndInstanceIdGreaterThanEqualAndInstanceIdLessThanEqual(UUID jobExecutionId, UUID fromId, UUID toId);
 
-  void deleteByJobExecutionId(UUID jobExecutionId);
+  @Modifying
+  @Query("DELETE ExportIdEntity e WHERE e.jobExecutionId = :jobExecutionId")
+  int deleteWithJobExecutionId(@Param("jobExecutionId") UUID jobExecutionId);
 
   default Slice<ExportIdEntity> getExportIds(UUID jobExecutionId, UUID fromId, UUID toId, Pageable page) {
     return findByJobExecutionIdIsAndInstanceIdGreaterThanEqualAndInstanceIdLessThanEqualOrderByInstanceIdAsc(jobExecutionId, fromId, toId, page);
