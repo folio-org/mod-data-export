87c81e0bad4041ec6e52d3098bbaae2c75823999
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-03-22 17:14:54 +0300

    MDEXP-741 - fix tests

--- src/test/java/org/folio/dataexp/service/MappingProfileServiceTest.java
+++ src/test/java/org/folio/dataexp/service/MappingProfileServiceTest.java
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MappingProfileServiceTest {
+class MappingProfileServiceTest {
   @Mock
   private FolioExecutionContext folioExecutionContext;
   @Mock
