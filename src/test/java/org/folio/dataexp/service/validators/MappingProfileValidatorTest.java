b6dbdabec7ade5b893d96e4a908a4024d640204b
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-04-15 15:07:59 +0300

    MDEXP-726 - update validation tests

--- src/test/java/org/folio/dataexp/service/validators/MappingProfileValidatorTest.java
+++ src/test/java/org/folio/dataexp/service/validators/MappingProfileValidatorTest.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class MappingProfileValidatorTest {
+class MappingProfileValidatorTest {
 
   @Test
   void validateMappingProfileTransformationsTest() {
