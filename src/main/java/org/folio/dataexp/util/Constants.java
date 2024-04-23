39257ceb7278628522c62b1f5e7156a17c93fab6
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-04-23 12:36:31 +0300

    MDEXP-682 Initial commit

--- src/main/java/org/folio/dataexp/util/Constants.java
+++ src/main/java/org/folio/dataexp/util/Constants.java
@@ -9,5 +9,5 @@ public class Constants {
   public static final String ERROR_MESSAGE_PLACEHOLDER_CODE = "error.messagePlaceholder";
   public static final String OKAPI_URL = "http://_";
   public static final String COMMA = ",";
-  public static final String DATE_PATTERN = "yyyy-MM-dd";
+  public static final String DATE_PATTERN = "yyyyMMdd";
 }
