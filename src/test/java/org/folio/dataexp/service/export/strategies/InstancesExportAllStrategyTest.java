7af45724196e06c1e37c0d27f065f5919e0f5830
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2024-04-26 15:24:09 +0300

    MDEXP-756 - "Error.recordIsTooLong" error is not displayed on Error log page for Custom profile with SRS (#479)
    * MDEXP-756 - errors saving before exception
    
    * MDEXP-756 - code refactoring
    
    * MDEXP-756 - aveConvertJsonRecordToMarcRecordError impl
    
    * MDEXP-756 - update tests
    
    * MDEXP-756 - update tests
    
    * MDEXP-756 - update tests
    
    * MDEXP-756 - update logging

--- src/test/java/org/folio/dataexp/service/export/strategies/InstancesExportAllStrategyTest.java
+++ src/test/java/org/folio/dataexp/service/export/strategies/InstancesExportAllStrategyTest.java
@@ -1,20 +1,29 @@
 package org.folio.dataexp.service.export.strategies;
 
+import net.minidev.json.JSONObject;
 import org.folio.dataexp.domain.entity.AuditInstanceEntity;
+import org.folio.dataexp.domain.entity.InstanceEntity;
+import org.folio.dataexp.domain.entity.MarcRecordEntity;
 import org.folio.dataexp.repository.AuditInstanceEntityRepository;
 import org.folio.dataexp.repository.InstanceEntityRepository;
+import org.folio.dataexp.service.logs.ErrorLogService;
+import org.folio.dataexp.util.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +33,8 @@ class InstancesExportAllStrategyTest {
   private AuditInstanceEntityRepository auditInstanceEntityRepository;
   @Mock
   private InstanceEntityRepository instanceEntityRepository;
+  @Mock
+  private ErrorLogService errorLogService;
 
   @InjectMocks
   private InstancesExportAllStrategy instancesExportAllStrategy;
@@ -56,5 +67,57 @@ class InstancesExportAllStrategyTest {
 
     assertTrue(opt.isPresent());
     assertEquals("Instance with ID : b9d26945-9757-4855-ae6e-fd5d2f7d778e", opt.get().getIdentifierHridMessage());
+  }
+
+  @Test
+  void saveConvertJsonRecordToMarcRecordErrorIfErrorRecordTooLongAndInstanceDeletedTest() {
+    instancesExportAllStrategy.setErrorLogService(errorLogService);
+
+    var auditInstanceEntity = AuditInstanceEntity.builder()
+      .id(UUID.randomUUID()).hrid("123").title("title").build();
+    var jobExecutionId = UUID.randomUUID();
+    var instanceId = UUID.fromString("1eaa1eef-1633-4c7e-af09-796315ebc576");
+    var marcRecord = MarcRecordEntity.builder().externalId(instanceId).build();
+    var errorMessage = "Record is too long to be a valid MARC binary record, it's length would be 113937 which is more thatn 99999 bytes 2024";
+
+    when(instanceEntityRepository.findByIdIn(anySet())).thenReturn(List.of());
+    when(auditInstanceEntityRepository.findByIdIn(anySet())).thenReturn(List.of(auditInstanceEntity));
+
+    instancesExportAllStrategy.saveConvertJsonRecordToMarcRecordError(marcRecord, jobExecutionId, new IOException(errorMessage));
+    verify(errorLogService).saveWithAffectedRecord(isA(JSONObject.class), eq(errorMessage), eq(ErrorCode.ERROR_MESSAGE_JSON_CANNOT_BE_CONVERTED_TO_MARC.getCode()), isA(UUID.class));
+  }
+
+  @Test
+  void saveConvertJsonRecordToMarcRecordErrorIfErrorRecordTooLongAndInstanceNotDeletedTest() {
+    instancesExportAllStrategy.setErrorLogService(errorLogService);
+
+    var jobExecutionId = UUID.randomUUID();
+    var instance = "{'id' : '1eaa1eef-1633-4c7e-af09-796315ebc576', 'hrid' : 'instHrid', 'title' : 'title'}";
+    var instanceId = UUID.fromString("1eaa1eef-1633-4c7e-af09-796315ebc576");
+    var instanceEntity = InstanceEntity.builder().jsonb(instance).id(instanceId).build();
+    var marcRecord = MarcRecordEntity.builder().externalId(instanceId).build();
+    var errorMessage = "Record is too long to be a valid MARC binary record, it's length would be 113937 which is more thatn 99999 bytes 2024";
+
+    when(instanceEntityRepository.findByIdIn(anySet())).thenReturn(List.of(instanceEntity));
+
+    instancesExportAllStrategy.saveConvertJsonRecordToMarcRecordError(marcRecord, jobExecutionId, new IOException(errorMessage));
+
+    verify(errorLogService).saveWithAffectedRecord(isA(JSONObject.class), eq(errorMessage), eq(ErrorCode.ERROR_MESSAGE_JSON_CANNOT_BE_CONVERTED_TO_MARC.getCode()), isA(UUID.class));
+  }
+
+
+  @Test
+  void saveConvertJsonRecordToMarcRecordErrorIfNotErrorRecordTooLongTest() {
+    instancesExportAllStrategy.setErrorLogService(errorLogService);
+
+    var jobExecutionId = UUID.randomUUID();
+    var instanceId = UUID.fromString("1eaa1eef-1633-4c7e-af09-796315ebc576");
+    var marcRecord = MarcRecordEntity.builder().externalId(instanceId).build();
+    var errorMessage = "error message";
+
+    instancesExportAllStrategy.saveConvertJsonRecordToMarcRecordError(marcRecord, jobExecutionId, new IOException(errorMessage));
+
+    var expectedErrorMessage = "Error converting json to marc for record 1eaa1eef-1633-4c7e-af09-796315ebc576";
+    verify(errorLogService).saveGeneralError(expectedErrorMessage, jobExecutionId);
   }
 }
