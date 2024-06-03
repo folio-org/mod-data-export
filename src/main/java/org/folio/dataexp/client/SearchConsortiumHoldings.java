1511ad6cab804a47542688bb7631575948fa3fe3
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-06-03 14:59:34 +0300

    MDEXP-695 - fix client

--- src/main/java/org/folio/dataexp/client/SearchConsortiumHoldings.java
+++ src/main/java/org/folio/dataexp/client/SearchConsortiumHoldings.java
@@ -2,7 +2,7 @@ package org.folio.dataexp.client;
 
 import org.folio.dataexp.domain.dto.ConsortiumHoldingCollection;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
@@ -12,6 +12,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @FeignClient(name = "search/consortium/holdings")
 public interface SearchConsortiumHoldings {
 
-  @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+  @GetMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
   ConsortiumHoldingCollection getHoldingsById(@RequestParam UUID instanceId);
 }
