218880e6ff0f3aad1737a74929e5f1fe943164b7
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-06-05 11:44:34 +0300

    MDEXP-751 Fixed search

--- src/main/java/org/folio/dataexp/client/ConsortiumSearchClient.java
+++ src/main/java/org/folio/dataexp/client/ConsortiumSearchClient.java
@@ -1,16 +1,15 @@
 package org.folio.dataexp.client;
 
 import org.folio.dataexp.domain.dto.Holdings;
-import org.folio.dataexp.domain.dto.HoldingsCollection;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @FeignClient(name = "search/consortium")
 public interface ConsortiumSearchClient {
 
-  @GetMapping(value = "/holdings", produces = APPLICATION_JSON_VALUE)
-  Holdings getHoldingsById(@RequestParam String holdingId);
+  @GetMapping(value = "/holding/{holdingId}", produces = APPLICATION_JSON_VALUE)
+  Holdings getHoldingsById(@PathVariable String holdingId);
 }
