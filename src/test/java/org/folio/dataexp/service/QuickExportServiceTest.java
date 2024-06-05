1a908d864d1699babbcef87e6d6800b338a4cbaa
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-06-05 15:26:20 +0300

    MDEXP-695 - fix sonar

--- src/test/java/org/folio/dataexp/service/QuickExportServiceTest.java
+++ src/test/java/org/folio/dataexp/service/QuickExportServiceTest.java
@@ -25,8 +25,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 class QuickExportServiceTest extends ServiceInitializer {
