3ffba036f9cf3be891c8f9b473aefeee8d28fe8b
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-07-24 15:11:09 +0300

    MDEXP-747 Fixed coverage

--- src/test/java/org/folio/dataexp/service/export/strategies/InstancesExportAllStrategyTest.java
+++ src/test/java/org/folio/dataexp/service/export/strategies/InstancesExportAllStrategyTest.java
@@ -1,23 +1,33 @@
 package org.folio.dataexp.service.export.strategies;
 
 import net.minidev.json.JSONObject;
+import org.folio.dataexp.domain.dto.JobExecution;
+import org.folio.dataexp.domain.dto.JobExecutionProgress;
+import org.folio.dataexp.domain.dto.MappingProfile;
 import org.folio.dataexp.domain.entity.AuditInstanceEntity;
 import org.folio.dataexp.domain.entity.InstanceEntity;
+import org.folio.dataexp.domain.entity.JobExecutionEntity;
 import org.folio.dataexp.domain.entity.MarcRecordEntity;
 import org.folio.dataexp.repository.AuditInstanceEntityRepository;
 import org.folio.dataexp.repository.InstanceEntityRepository;
+import org.folio.dataexp.repository.JobExecutionEntityRepository;
+import org.folio.dataexp.service.export.LocalStorageWriter;
 import org.folio.dataexp.service.logs.ErrorLogService;
 import org.folio.dataexp.util.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.ap.internal.util.Collections;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
+import static org.folio.dataexp.util.ErrorCode.ERROR_DELETED_DUPLICATED_INSTANCE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anySet;
@@ -34,7 +44,15 @@ class InstancesExportAllStrategyTest {
   @Mock
   private InstanceEntityRepository instanceEntityRepository;
   @Mock
+  private JobExecutionEntityRepository jobExecutionEntityRepository;
+  @Mock
   private ErrorLogService errorLogService;
+  @Mock
+  private LocalStorageWriter localStorageWriter;
+  @Mock
+  private HoldingsItemsResolverService holdingsItemsResolver;
+  @Mock
+  private JsonToMarcConverter jsonToMarcConverter;
 
   @InjectMocks
   private InstancesExportAllStrategy instancesExportAllStrategy;
@@ -85,6 +103,28 @@ class InstancesExportAllStrategyTest {
 
     instancesExportAllStrategy.saveConvertJsonRecordToMarcRecordError(marcRecord, jobExecutionId, new IOException(errorMessage));
     verify(errorLogService).saveWithAffectedRecord(isA(JSONObject.class), eq(errorMessage), eq(ErrorCode.ERROR_MESSAGE_JSON_CANNOT_BE_CONVERTED_TO_MARC.getCode()), isA(UUID.class));
+  }
+
+  @Test
+  void saveDuplicateErrorsIfMarcDeletedTest() {
+    instancesExportAllStrategy.setErrorLogService(errorLogService);
+
+    ReflectionTestUtils.setField(instancesExportAllStrategy, "jsonToMarcConverter", jsonToMarcConverter);
+    var auditInstanceEntity = AuditInstanceEntity.builder()
+      .id(UUID.randomUUID()).hrid("123").title("title").build();
+    var jobExecutionId = UUID.randomUUID();
+    var instanceId = UUID.fromString("1eaa1eef-1633-4c7e-af09-796315ebc576");
+    var marcRecord = MarcRecordEntity.builder().externalId(instanceId).id(UUID.randomUUID()).deleted(true).build();
+    var marcRecordDuplicate = MarcRecordEntity.builder().externalId(instanceId).id(UUID.randomUUID()).build();
+    var statistic = new ExportStrategyStatistic(new ExportedMarcListener(jobExecutionEntityRepository, 1, jobExecutionId));
+    var externalIds = Collections.asSet(instanceId);
+
+    when(instanceEntityRepository.findByIdIn(anySet())).thenReturn(List.of());
+    when(jobExecutionEntityRepository.getReferenceById(jobExecutionId)).thenReturn(new JobExecutionEntity().withJobExecution(new JobExecution().progress(new JobExecutionProgress())));
+    when(auditInstanceEntityRepository.findByIdIn(anySet())).thenReturn(List.of(auditInstanceEntity));
+
+    instancesExportAllStrategy.createAndSaveMarcFromJsonRecord(externalIds, statistic, new MappingProfile(), jobExecutionId, Set.of(instanceId), List.of(marcRecord, marcRecordDuplicate), localStorageWriter);
+    verify(errorLogService).saveGeneralErrorWithMessageValues(eq(ERROR_DELETED_DUPLICATED_INSTANCE.getCode()), eq(List.of(marcRecord.getId().toString())), isA(UUID.class));
   }
 
   @Test
