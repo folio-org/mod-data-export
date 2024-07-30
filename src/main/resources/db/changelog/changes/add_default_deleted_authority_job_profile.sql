2115f4706b42cb62040ce1c42f74e02315618df0
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-07-30 15:45:53 +0400

    MDEXP-758 - add_default_deleted_authority_job_profile.sql

---
+++ src/main/resources/db/changelog/changes/add_default_deleted_authority_job_profile.sql
@@ -0,0 +1,36 @@
+INSERT INTO job_profiles(id, jsonb, creation_date, created_by, mappingprofileid,
+name, description, updated_date, updated_by_user_id, updated_by_first_name, updated_by_last_name)
+VALUES
+( '2c9be114-6d35-4408-adac-9ead35f51a27',
+  '{
+     "id": "2c9be114-6d35-4408-adac-9ead35f51a27",
+     "name": "Deleted authority export job profile",
+     "default" : true,
+     "destination": "fileSystem",
+     "description": "Deleted authority export job profile",
+     "userInfo": {
+       "firstName": "System",
+       "lastName": "Process",
+       "userName": "system_process"
+     },
+     "mappingProfileId": "5d636597-a59d-4391-a270-4e79d5ba70e3",
+     "metadata":{
+       "createdDate":"2024-03-14T00:00:00Z",
+       "createdByUserId":"00000000-0000-0000-0000-000000000000",
+       "createdByUsername":"system_process",
+       "updatedDate":"2024-03-14T00:00:00Z",
+       "updatedByUserId":"00000000-0000-0000-0000-000000000000",
+       "updatedByUsername":"system_process"
+     }
+   }',
+  '2024-03-14T00:00:00Z',
+  '00000000-0000-0000-0000-000000000000',
+  '5d636597-a59d-4391-a270-4e79d5ba70e3',
+  'Deleted authority export job profile',
+  'Deleted authority export job profile',
+  '2024-03-14T00:00:00Z',
+  '00000000-0000-0000-0000-000000000000',
+  'System',
+  'Process'
+) ON CONFLICT DO NOTHING;
+
