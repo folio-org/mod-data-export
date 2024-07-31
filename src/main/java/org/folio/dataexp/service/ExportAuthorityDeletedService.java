ca23b062677a6cf8c886898b3e8ac5c8762e5f50
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-07-31 09:22:26 +0300

    MDEXP-769 Fix sonar

--- src/main/java/org/folio/dataexp/service/ExportAuthorityDeletedService.java
+++ src/main/java/org/folio/dataexp/service/ExportAuthorityDeletedService.java
@@ -3,6 +3,7 @@ package org.folio.dataexp.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.dataexp.client.AuthorityClient;
+import org.folio.dataexp.domain.dto.Authority;
 import org.folio.dataexp.domain.dto.ExportAuthorityDeletedRequest;
 import org.folio.dataexp.domain.dto.ExportAuthorityDeletedResponse;
 import org.folio.dataexp.domain.dto.ExportRequest;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
-import static org.folio.dataexp.util.Constants.DEFAULT_AUTHORITY_DELETED_JOB_PROFILE_ID;
+import static org.folio.dataexp.util.Constants.AUTHORITY_DELETED_JOB_PROFILE_ID;
 import static org.folio.dataexp.util.Constants.DELETED_AUTHORITIES_FILE_NAME;
 
 @Log4j2
@@ -30,8 +31,8 @@ public class ExportAuthorityDeletedService {
     log.info("POST export deleted authorities");
     var authorities = authorityClient.getAuthorities(true, true, request.getQuery(), request.getLimit(),
       request.getOffset());
-    var fileDefinition = getFileDefinition(authorities.getAuthorities().stream().map(auth -> auth.getId()).toList());
-    var exportRequest = ExportRequest.builder().fileDefinitionId(fileDefinition.getId()).jobProfileId(UUID.fromString(DEFAULT_AUTHORITY_DELETED_JOB_PROFILE_ID))
+    var fileDefinition = getFileDefinition(authorities.getAuthorities().stream().map(Authority::getId).toList());
+    var exportRequest = ExportRequest.builder().fileDefinitionId(fileDefinition.getId()).jobProfileId(UUID.fromString(AUTHORITY_DELETED_JOB_PROFILE_ID))
       .all(false).quick(false).idType(ExportRequest.IdTypeEnum.AUTHORITY).build();
     dataExportService.postDataExport(exportRequest);
     return ExportAuthorityDeletedResponse.builder().jobExecutionId(fileDefinition.getJobExecutionId()).build();
