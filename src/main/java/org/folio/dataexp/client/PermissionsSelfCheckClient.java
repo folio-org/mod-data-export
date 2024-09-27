296b315f711b6b16723768ae2c9f8c2522cede55
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-09-27 14:21:40 +0300

    MDEXP-786 Test

--- src/main/java/org/folio/dataexp/client/PermissionsSelfCheckClient.java
+++ src/main/java/org/folio/dataexp/client/PermissionsSelfCheckClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "bulk-edit", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "data-export", configuration = FeignClientConfiguration.class)
 public interface PermissionsSelfCheckClient {
 
   @GetMapping(value = "/permissions-self-check", produces = MediaType.APPLICATION_JSON_VALUE)
