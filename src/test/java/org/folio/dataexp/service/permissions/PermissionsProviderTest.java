4e77a24782d3159fc598e801f1fdb7b8dbca2f89
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-10-04 22:20:16 +0300

    MDEXP-786 Added test

---
+++ src/test/java/org/folio/dataexp/service/permissions/PermissionsProviderTest.java
@@ -0,0 +1,39 @@
+package org.folio.dataexp.service.permissions;
+
+
+import org.folio.dataexp.client.PermissionsSelfCheckClient;
+import org.folio.okapi.common.XOkapiHeaders;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.FolioModuleMetadata;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PermissionsProviderTest {
+
+  @Mock
+  private PermissionsSelfCheckClient permissionsSelfCheckClient;
+  @Mock
+  private FolioExecutionContext folioExecutionContext;
+  @Mock
+  private FolioModuleMetadata folioModuleMetadata;
+  @InjectMocks
+  private PermissionsProvider permissionsProvider;
+
+  @Test
+  void getUserPermissionsTest() {
+    when(permissionsSelfCheckClient.getUserPermissionsForSelfCheck()).thenReturn(List.of("some permission"));
+    when(folioExecutionContext.getOkapiHeaders()).thenReturn(Map.of(XOkapiHeaders.TENANT, List.of("college")));
+    var userPerms = permissionsProvider.getUserPermissions("college");
+    assertEquals("some permission", userPerms.get(0));
+  }
+}
