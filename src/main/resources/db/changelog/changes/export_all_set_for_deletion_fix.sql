942da95975ab815b1f077cdc09a51b459bf45f5c
siarhei-charniak <siarhei_charniak@epam.com>
2024-10-14 17:34:45 +0300

    minor fixes

---
+++ src/main/resources/db/changelog/changes/export_all_set_for_deletion_fix.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE VIEW ${myuniversity}_mod_data_export.v_folio_instance_all_non_deleted
+AS SELECT inst.id, jsonb FROM ${myuniversity}_mod_data_export.v_instance inst
+   WHERE jsonb ->> 'source' = 'FOLIO' AND
+     ((jsonb ->> 'dicscoverySuppress' IS NULL OR jsonb ->> 'discoverySuppress' = 'false') OR
+      (jsonb ->> 'staffSuppress' IS NULL OR jsonb ->> 'staffSuppress' = 'false'));
