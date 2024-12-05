c303abb86304209f7c9e355ce209ec11cc858ac3
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2024-12-05 16:48:43 +0300

    EUREKA-529 - avoid self invocation (#524)

--- src/main/java/org/folio/dataexp/service/validators/PermissionsValidator.java
+++ src/main/java/org/folio/dataexp/service/validators/PermissionsValidator.java
@@ -1,7 +1,6 @@
 package org.folio.dataexp.service.validators;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.folio.dataexp.service.permissions.PermissionsProvider;
 import org.folio.dataexp.service.permissions.RequiredPermissionResolver;
 import org.folio.spring.FolioExecutionContext;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Log4j2
 public class PermissionsValidator {
 
   private final PermissionsProvider permissionsProvider;
@@ -23,9 +21,6 @@ public class PermissionsValidator {
   public boolean isInstanceViewPermissionExists(String tenantId) {
     var readPermissionForEntity = requiredPermissionResolver.getReadPermission();
     var userPermissions = permissionsProvider.getUserPermissions(tenantId, folioExecutionContext.getUserId().toString());
-    var isViewPermissionsExist = userPermissions.contains(readPermissionForEntity);
-    log.info("isInstanceViewPermissionExists:: user {} has read permissions {} in tenant {}", folioExecutionContext.getUserId(),
-      isViewPermissionsExist, tenantId);
-    return isViewPermissionsExist;
+    return userPermissions.contains(readPermissionForEntity);
   }
 }
