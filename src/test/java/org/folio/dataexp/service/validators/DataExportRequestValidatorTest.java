551a9f06466a5605e6ace1e20fafc84e64007de4
siarhei-charniak <siarhei_charniak@epam.com>
2025-06-26 12:53:01 +0300

    MDEXP-836 - BE - Export authority records using CQL

--- src/test/java/org/folio/dataexp/service/validators/DataExportRequestValidatorTest.java
+++ src/test/java/org/folio/dataexp/service/validators/DataExportRequestValidatorTest.java
@@ -43,28 +43,6 @@ class DataExportRequestValidatorTest {
   }
 
   @Test
-  void validateAuthorityExportRequestTest() {
-    when(errorLogService.saveGeneralErrorWithMessageValues("error.messagePlaceholder",
-      List.of("For exporting authority records only the default authority job profile is supported"), null))
-      .thenReturn(new ErrorLog());
-    var validator = new DataExportRequestValidator(errorLogService);
-    var fileDefinition = new FileDefinition();
-    fileDefinition.setId(UUID.randomUUID());
-
-    fileDefinition.setSize(500_000);
-
-    var exportRequest = new ExportRequest();
-    exportRequest.setIdType(ExportRequest.IdTypeEnum.AUTHORITY);
-
-    assertThrows(DataExportRequestValidationException.class, () -> validator.validate(exportRequest, fileDefinition, "uuid"));
-
-    fileDefinition.fileName("upload.cql");
-    fileDefinition.setUploadFormat(FileDefinition.UploadFormatEnum.CQL);
-    var defaultAuthorityMappingProfile = "5d636597-a59d-4391-a270-4e79d5ba70e3";
-    assertThrows(DataExportRequestValidationException.class, () -> validator.validate(exportRequest, fileDefinition, defaultAuthorityMappingProfile));
-  }
-
-  @Test
   void validateAuthorityDeletedProfileExportRequestTest() {
     when(errorLogService.saveGeneralErrorWithMessageValues("error.onlyForSetToDeletion",
       List.of("This profile can only be used to export authority records set for deletion"), null))
