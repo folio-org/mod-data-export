376175f5d76468d5bffbd4c1658067f29576e1c5
Ryan Lee <rlee1@ebsco.com>
2025-10-01 11:09:03 -0700

    Add required annotations

--- src/main/java/org/folio/dataexp/service/export/strategies/ld/LinkedDataExportStrategy.java
+++ src/main/java/org/folio/dataexp/service/export/strategies/ld/LinkedDataExportStrategy.java
@@ -3,9 +3,15 @@ package org.folio.dataexp.service.export.strategies.ld;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.dataexp.domain.dto.ExportRequest;
 import org.folio.dataexp.domain.dto.MappingProfile;
+import org.springframework.stereotype.Component;
 
+@Log4j2
+@Component
+@RequiredArgsConstructor
 public class LinkedDataExportStrategy extends AbstractLinkedDataExportStrategy {
 
   private LinkedDataProvider linkedDataProvider;
