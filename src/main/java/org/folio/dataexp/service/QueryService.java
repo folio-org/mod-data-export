ef82c72a351eeb476e7d4b2fbca30811e0c1c990
Ryan Lee <rlee1@ebsco.com>
2025-10-01 15:39:02 -0700

    checkstyle

--- src/main/java/org/folio/dataexp/service/QueryService.java
+++ src/main/java/org/folio/dataexp/service/QueryService.java
@@ -11,6 +11,9 @@ import org.folio.dataexp.client.QueryClient;
 import org.folio.querytool.domain.dto.ContentsRequest;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for retrieving entities from FQM.
+ */
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -18,19 +21,29 @@ public class QueryService {
 
   private final QueryClient queryClient;
 
-  public List<Map<String, Object>> getEntities(Set<UUID> ids, UUID entityTypeId, List<String> fields) {
+  /**
+   * Retrieves entities from FQM based on identifiers. Callers should expect
+   * to receive the generic return list and then process further by entity type.
+   *
+   * @param ids list of identifiers for entities to retrieve
+   * @param entityTypeId FQM entity type UUID
+   * @param fields entity fields to retrieve
+   * @return list of entities as field-value maps
+   */
+  public List<Map<String, Object>> getEntities(Set<UUID> ids,
+      UUID entityTypeId, List<String> fields) {
     var request = toRequest(ids, entityTypeId, fields);
     return runQuery(request);
   }
 
   private ContentsRequest toRequest(Set<UUID> ids, UUID entityTypeId, List<String> fields) {
     var requestIds = ids.stream()
-      .map(id -> Collections.singletonList(id.toString()))
-      .toList();
+        .map(id -> Collections.singletonList(id.toString()))
+        .toList();
     return new ContentsRequest()
-      .ids(requestIds)
-      .entityTypeId(entityTypeId)
-      .fields(fields);
+        .ids(requestIds)
+        .entityTypeId(entityTypeId)
+        .fields(fields);
   }
 
   private List<Map<String, Object>> runQuery(ContentsRequest contents) {
