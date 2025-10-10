015fc56954e507da5db3d4e831141c5eb98fd2ef
Ryan Lee <rlee1@ebsco.com>
2025-10-09 18:09:46 -0700

    Add basic AbstractLinkedDataExportStrategy unit test with mocked converter for now

---
+++ src/test/java/org/folio/dataexp/service/export/strategies/ld/AbstractLinkedDataExportStrategyTest.java
@@ -0,0 +1,148 @@
+package org.folio.dataexp.service.export.strategies.ld;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import org.folio.dataexp.domain.dto.ExportRequest;
+import org.folio.dataexp.domain.dto.JobExecution;
+import org.folio.dataexp.domain.dto.JobExecutionProgress;
+import org.folio.dataexp.domain.dto.LinkedDataResource;
+import org.folio.dataexp.domain.entity.ExportIdEntity;
+import org.folio.dataexp.domain.entity.JobExecutionEntity;
+import org.folio.dataexp.domain.entity.JobExecutionExportFilesEntity;
+import org.folio.dataexp.domain.entity.JobExecutionExportFilesStatus;
+import org.folio.dataexp.domain.entity.JobProfileEntity;
+import org.folio.dataexp.repository.ExportIdEntityRepository;
+import org.folio.dataexp.repository.JobExecutionEntityRepository;
+import org.folio.dataexp.repository.JobExecutionExportFilesEntityRepository;
+import org.folio.dataexp.repository.JobProfileEntityRepository;
+import org.folio.dataexp.service.JobExecutionService;
+import org.folio.dataexp.service.export.LocalStorageWriter;
+import org.folio.dataexp.service.export.strategies.ExportedMarcListener;
+import org.folio.dataexp.service.logs.ErrorLogService;
+import org.folio.dataexp.util.ErrorCode;
+import org.folio.s3.client.FolioS3Client;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
+
+@ExtendWith(MockitoExtension.class)
+class AbstractLinkedDataExportStrategyTest {
+
+  @Mock
+  private FolioS3Client s3Client;
+  @Mock
+  private JobExecutionExportFilesEntityRepository jobExecutionExportFilesEntityRepository;
+  @Mock
+  private ExportIdEntityRepository exportIdEntityRepository;
+  @Mock
+  private JobProfileEntityRepository jobProfileEntityRepository;
+  @Mock
+  private JobExecutionEntityRepository jobExecutionEntityRepository;
+  @Mock
+  private JobExecutionService jobExecutionService;
+  @Mock
+  private LocalStorageWriter localStorageWriter;
+  @Mock
+  private ErrorLogService errorLogService;
+  @Mock
+  private LinkedDataConverter linkedDataConverter;
+
+  @InjectMocks
+  private AbstractLinkedDataExportStrategy exportStrategy = new LDTestExportStrategy(1);
+
+  @SneakyThrows
+  @Test
+  void saveOutputToLocalStorageTest() {
+    MockitoAnnotations.openMocks(this);
+
+    var progress = new JobExecutionProgress();
+    var jobExecution = JobExecution.builder().progress(progress).id(UUID.randomUUID()).build();
+    var jobProfileEntity = new JobProfileEntity();
+    jobProfileEntity.setId(UUID.randomUUID());
+    jobExecution.setId(UUID.randomUUID());
+    jobExecution.setJobProfileId(jobProfileEntity.getId());
+
+    var linkedDataResources = new ArrayList<LinkedDataResource>();
+    var linkedDataResource = new LinkedDataResource();
+    var exportId = UUID.randomUUID();
+    linkedDataResource.setInventoryId(exportId.toString());
+    linkedDataResource.setResource("""
+      {
+        "id": "-8148541629405416677",
+        "label": "B",
+        "types": ["http://bibfra.me/vocab/lite/Instance"],
+        "outgoingEdges": {}
+      }""");
+    linkedDataResources.add(linkedDataResource);
+    ((LDTestExportStrategy) exportStrategy).setLinkedDataResources(linkedDataResources);
+
+    JobExecutionExportFilesEntity exportFilesEntity = new JobExecutionExportFilesEntity()
+        .withFileLocation("/tmp/" + jobExecution.getId().toString() + "/location")
+        .withId(UUID.randomUUID()).withJobExecutionId(jobExecution.getId())
+        .withFromId(UUID.randomUUID()).withToId(UUID.randomUUID())
+        .withStatus(JobExecutionExportFilesStatus.ACTIVE);
+    var exportIdEntity = new ExportIdEntity()
+        .withJobExecutionId(exportFilesEntity.getJobExecutionId())
+        .withId(0).withInstanceId(exportId);
+    var slice = new SliceImpl<>(List.of(exportIdEntity), PageRequest.of(0, 1), false);
+
+    when(exportIdEntityRepository.getExportIds(isA(UUID.class), isA(UUID.class), isA(UUID.class),
+          isA(Pageable.class))).thenReturn(slice);
+    var jobExecutionEntity = JobExecutionEntity.fromJobExecution(jobExecution);
+    when(jobExecutionEntityRepository.getReferenceById(isA(UUID.class)))
+        .thenReturn(jobExecutionEntity);
+    var output = new ByteArrayOutputStream(2);
+    output.write("{}".getBytes());
+    when(linkedDataConverter.convertLdJsonToBibframe2Rdf(isA(String.class)))
+        .thenReturn(output);
+
+    var exportStatistic = exportStrategy.saveOutputToLocalStorage(exportFilesEntity,
+        new ExportRequest(), new ExportedMarcListener(jobExecutionEntityRepository,
+            1, jobExecutionEntity.getId()));
+    assertEquals(1, exportStatistic.getExported());
+    assertEquals(0, exportStatistic.getDuplicatedSrs());
+    assertEquals(0, exportStatistic.getFailed());
+
+    assertEquals(JobExecutionExportFilesStatus.ACTIVE, exportFilesEntity.getStatus());
+    verify(jobExecutionEntityRepository, times(1))
+        .save(isA(JobExecutionEntity.class));
+    verify(localStorageWriter, times(1)).write(isA(String.class));
+  }
+
+  class LDTestExportStrategy extends AbstractLinkedDataExportStrategy {
+    LDTestExportStrategy(int exportBatch) {
+      super.setExportIdsBatch(exportBatch);
+    }
+
+    @Setter
+    private List<LinkedDataResource> linkedDataResources = new ArrayList<>();
+
+    @Override
+    protected LocalStorageWriter createLocalStorageWriter(
+        JobExecutionExportFilesEntity exportFilesEntity) {
+      return localStorageWriter;
+    }
+
+    @Override
+    List<LinkedDataResource> getLinkedDataResources(Set<UUID> externalIds) {
+      return linkedDataResources;
+    }
+  }
+}
