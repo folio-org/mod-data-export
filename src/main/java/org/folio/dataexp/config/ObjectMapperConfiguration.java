af7ca5d084820afab0b3c4e614f503dd1e3413e0
Ryan Lee <rlee1@ebsco.com>
2025-11-05 16:23:51 -0800

    Reuse Spring autoconfiguration for ObjectMapper

--- src/main/java/org/folio/dataexp/config/ObjectMapperConfiguration.java
+++ src/main/java/org/folio/dataexp/config/ObjectMapperConfiguration.java
@@ -1,11 +1,10 @@
 package org.folio.dataexp.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * Configuration class for JSON object mapping.
@@ -19,10 +18,7 @@ public class ObjectMapperConfiguration {
    */
   @Bean
   @Primary
-  public ObjectMapper dataExportObjectMapper() {
-    return new ObjectMapper()
-      .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
-      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+  ObjectMapper dataExportObjectMapper(Jackson2ObjectMapperBuilder builder) {
+    return builder.build();
   }
 }
