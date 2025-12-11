be91fb31264aabd6b4001ec448a861646edd343d
OleksandrBozhko <Oleksandr_Bozhko@epam.com>
2025-12-11 15:48:30 +0200

    MDEXP-861 Fixed issue with first upload
    (cherry picked from commit 22a75b21aaf3b3942bfd75297b82271614ed613c)

--- src/main/java/org/folio/dataexp/service/DownloadRecordService.java
+++ src/main/java/org/folio/dataexp/service/DownloadRecordService.java
@@ -1,9 +1,10 @@
 package org.folio.dataexp.service;
 
-
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -14,9 +15,17 @@ import org.folio.dataexp.service.export.ExportStrategyFactory;
 import org.folio.dataexp.service.export.S3ExportsUploader;
 import org.folio.dataexp.service.export.strategies.JsonToMarcConverter;
 import org.folio.spring.FolioExecutionContext;
+import org.marc4j.MarcReader;
+import org.marc4j.MarcStreamReader;
+import org.marc4j.MarcStreamWriter;
+import org.marc4j.converter.impl.UnicodeToAnsel;
+import org.marc4j.marc.DataField;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for downloading MARC records by ID.
+ */
 @Service
 @AllArgsConstructor
 @Log4j2
@@ -28,47 +37,127 @@ public class DownloadRecordService {
   private final InputFileProcessor inputFileProcessor;
   protected final FolioExecutionContext folioExecutionContext;
 
-  public InputStreamResource processRecordDownload(final UUID recordId,
-                                                 boolean isUtf,
-                                                 final String formatPostfix,
-                                                 final IdType idType) {
-    log.info("processRecordDownload:: start downloading record with id: {}, isUtf: {}", recordId, isUtf);
+  /**
+   * Processes the download of a record by its ID.
+   *
+   * @param recordId The record UUID.
+   * @param isUtf Whether to use UTF encoding.
+   * @param formatPostfix Format postfix for the file.
+   * @param idType The type of ID.
+   * @return InputStreamResource containing the record data.
+   */
+  public InputStreamResource processRecordDownload(
+      final UUID recordId,
+      boolean isUtf,
+      final String formatPostfix,
+      final IdType idType,
+      boolean suppress999ff
+  ) {
+    log.info(
+        "processRecordDownload:: start downloading record with id: {}, "
+                + "isUtf: {}, suppress999ff: {}",
+        recordId,
+        isUtf,
+        suppress999ff
+    );
     var dirName = recordId.toString() + formatPostfix;
     InputStream marcFileContent = getContentIfFileExists(dirName);
     if (marcFileContent == null) {
       byte[] marcFileContentBytes = generateRecordFileContentBytes(recordId, isUtf, idType);
       uploadMarcFile(dirName, marcFileContentBytes);
+      if (suppress999ff) {
+        var inputStreamWithRemoved999ff = remove999ffField(isUtf,
+                new ByteArrayInputStream(marcFileContentBytes));
+        return new InputStreamResource(inputStreamWithRemoved999ff);
+      }
       return new InputStreamResource(new ByteArrayInputStream(marcFileContentBytes));
-    }
-    else {
+    } else {
+      if (suppress999ff) {
+        marcFileContent = remove999ffField(isUtf, marcFileContent);
+      }
       return new InputStreamResource(marcFileContent);
     }
-
   }
 
+  /**
+   * Gets the content of a MARC file if it exists.
+   *
+   * @param dirName Directory name.
+   * @return InputStream of the file, or null if not found.
+   */
   private InputStream getContentIfFileExists(final String dirName) {
     return inputFileProcessor.readMarcFile(dirName);
   }
 
-  private byte[] generateRecordFileContentBytes(final UUID recordId, boolean isUtf, final IdType idType) {
+  /**
+   * Generates the MARC file content bytes for a record.
+   *
+   * @param recordId The record UUID.
+   * @param isUtf Whether to use UTF encoding.
+   * @param idType The type of ID.
+   * @return Byte array of the MARC file content.
+   */
+  private byte[] generateRecordFileContentBytes(
+      final UUID recordId,
+      boolean isUtf,
+      final IdType idType
+  ) {
     var exportStrategy = exportStrategyFactory.getExportStrategy(idType);
     var marcRecord = exportStrategy.getMarcRecord(recordId);
     var mappingProfile = exportStrategy.getDefaultMappingProfile();
     try {
-      return jsonToMarcConverter.convertJsonRecordToMarcRecord(marcRecord.getContent(), List.of(), mappingProfile,
-        isUtf).toByteArray();
+      return jsonToMarcConverter.convertJsonRecordToMarcRecord(
+          marcRecord.getContent(),
+          List.of(),
+          mappingProfile,
+          isUtf
+      ).toByteArray();
     } catch (IOException e) {
-      log.error("generateRecordFileContent :: Error generating content for record with ID: {}", recordId);
+      log.error(
+          "generateRecordFileContent :: Error generating content for record with ID: {}",
+          recordId
+      );
       throw new DownloadRecordException(e.getMessage());
     }
   }
 
+  /**
+   * Uploads the MARC file to remote storage.
+   *
+   * @param dirName Directory name.
+   * @param marcFileContentBytes Byte array of MARC file content.
+   */
   private void uploadMarcFile(final String dirName, byte[] marcFileContentBytes) {
     try {
       s3Uploader.uploadSingleRecordById(dirName, marcFileContentBytes);
     } catch (IOException e) {
-      log.error("uploadMarcFile:: Error while upload marc file to remote storage {}", dirName);
+      log.error(
+          "uploadMarcFile:: Error while upload marc file to remote storage {}",
+          dirName
+      );
       throw new DownloadRecordException(e.getMessage());
+    }
+  }
+
+  private InputStream remove999ffField(boolean isUtf, InputStream marcFileContent) {
+    try (var marcOutputStream = new ByteArrayOutputStream()) {
+      var marcWriter = new MarcStreamWriter(marcOutputStream, StandardCharsets.UTF_8.name());
+      if (!isUtf) {
+        marcWriter.setConverter(new UnicodeToAnsel());
+      }
+      MarcReader marcReader = new MarcStreamReader(marcFileContent);
+      while (marcReader.hasNext()) {
+        var marcRecord = marcReader.next();
+        var fieldToRemove = marcRecord.getVariableFields().stream()
+                .filter(vf -> vf instanceof DataField df && df.getTag().equals("999")
+                && df.getIndicator1() == 'f' && df.getIndicator2() == 'f').findFirst();
+        fieldToRemove.ifPresent(marcRecord::removeVariableField);
+        marcWriter.write(marcRecord);
+      }
+      return new ByteArrayInputStream(marcOutputStream.toByteArray());
+    } catch (IOException e) {
+      log.error("Failed to remove tag {} from marc record: {}", "999ff", e.getMessage());
+      return marcFileContent;
     }
   }
 }
