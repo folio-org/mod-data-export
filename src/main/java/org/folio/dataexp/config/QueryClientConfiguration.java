d7787ed058b204b35da8952480b56352c44f81a0
Ryan Lee <rlee1@ebsco.com>
2026-01-08 13:28:59 -0800

    Remove decoder used for testing, switch retry values to configured variables

--- src/main/java/org/folio/dataexp/config/QueryClientConfiguration.java
+++ src/main/java/org/folio/dataexp/config/QueryClientConfiguration.java
@@ -1,33 +1,32 @@
 package org.folio.dataexp.config;
 
 import feign.Retryer;
-import feign.codec.ErrorDecoder;
-import org.folio.dataexp.util.FqmErrorDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 /** Additional configuration for the FQM Feign client, adding a retryer. */
 public class QueryClientConfiguration {
-  
+
+  @Value("${application.feign-query-cilent-retry.initial-wait-time}")
+  private int initialWaitTime;
+
+  @Value("${application.feign-query-cilent-retry.max-wait-time}")
+  private int maxWaitTime;
+
+  @Value("${application.feign-query-cilent-retry.max-attempts}")
+  private int maxAttempts;
+
   /**
-   * Add retries based on the Feign Retryer, starting at a 5s delay and
-   * increasing exponentially between subsequent retries. This should try
-   * again for about 2 minutes before failing.
-   * 
+   * Add retries based on the Feign Retryer. See environment variables for configuring initial time
+   * to wait before retrying, maximum wait time to allow as subsequent waits increase through
+   * exponential backoff, and maximum number of attempts before considering the request failed.
+   * Defaults of 5s, 30s, and 7 attempts should grant about 2 minutes to allow FQM to resume
+   * operations.
+   *
    * @return Retry algorithm for use with Feign client
    */
   @Bean
   public Retryer retryer() {
-    return new Retryer.Default(5000, 30000, 7);
-  }
-
-  /**
-   * Use a custom error decoder to interpret additional HTTP error codes as
-   * retryable beyond the default for HTTP 503.
-   *
-   * @return Feign error decoder
-   */
-  @Bean
-  public ErrorDecoder errorDecoder() {
-    return new FqmErrorDecoder();
+    return new Retryer.Default(initialWaitTime, maxWaitTime, maxAttempts);
   }
 }
