e4ee10e8f0a27396354c989c2481124718903885
OleksandrBozhko <Oleksandr_Bozhko@epam.com>
2026-01-15 22:00:13 +0200

    MDEXP-895 Update jsonb

--- src/main/resources/db/changelog/changes/add_locked_by_at_to_job_profile.sql
+++ src/main/resources/db/changelog/changes/add_locked_by_at_to_job_profile.sql
@@ -1,6 +1,12 @@
 ALTER TABLE job_profiles ADD COLUMN IF NOT EXISTS locked_by UUID;
 ALTER TABLE job_profiles ADD COLUMN IF NOT EXISTS locked_at TIMESTAMP;
-UPDATE job_profiles SET locked = true, jsonb = jsonb_set(jsonb, '{locked}', 'true', true)
+UPDATE job_profiles SET locked = true, locked_by = '00000000-0000-0000-0000-000000000000',
+                        locked_at = NOW(),
+                        jsonb = jsonb_set(
+                            jsonb_set(
+                                jsonb_set(jsonb, '{locked}', 'true', true),
+                                '{lockedBy}', '"00000000-0000-0000-0000-000000000000"', true),
+                            '{lockedAt}', to_jsonb(NOW()), true)
                     WHERE id IN (
 '56944b1c-f3f9-475b-bed0-7387c33620ce',
 '2c9be114-6d35-4408-adac-9ead35f51a27',
