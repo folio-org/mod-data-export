89c7c763edf7fb5cfa3cc3597878c782cb709983
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2026-02-14 10:28:29 +0300

    Unit tests refactoring

--- src/test/java/org/folio/dataexp/service/validators/DataExportRequestValidatorIT.java
+++ src/test/java/org/folio/dataexp/service/validators/DataExportRequestValidatorIT.java
@@ -17,7 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class DataExportRequestValidatorTest {
+class DataExportRequestValidatorIT {
 
   @Mock private ErrorLogService errorLogService;
 
