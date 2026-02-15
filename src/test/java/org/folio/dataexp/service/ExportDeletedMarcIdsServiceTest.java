49403005e4a38b4ef096ea21bdf75ae487c195b6
aitestmate <ai_testmate@epam.com>
2026-02-15 10:07:02 +0000

    [chore] [mod-data-export] unit tests

--- src/test/java/org/folio/dataexp/service/ExportDeletedMarcIdsServiceTest.java
+++ src/test/java/org/folio/dataexp/service/ExportDeletedMarcIdsServiceTest.java
@@ -55,4 +55,27 @@ class ExportDeletedMarcIdsServiceTest {
     assertThat(exportRequest.getFileDefinitionId()).isInstanceOf(UUID.class);
     assertThat(exportRequest.getJobProfileId()).isInstanceOf(UUID.class);
   }
+
+    @Test
+  void testPostExportDeletedMarcIdsWhenRequestIsNullShouldCallDependenciesWithNullDates() {
+    // TestMate-ac8197c64029dbb2bb834f4eecee6f2f
+    // Given
+    var fileDefinitionId = UUID.fromString("a1b2c3d4-e5f6-7890-1234-567890abcdef");
+    var jobExecutionId = UUID.fromString("f0e9d8c7-b6a5-4321-fedc-ba9876543210");
+    var fileDefinition = new FileDefinition().id(fileDefinitionId).jobExecutionId(jobExecutionId);
+    when(marcDeletedIdsService.getFileDefinitionForMarcDeletedIds(null, null))
+        .thenReturn(fileDefinition);
+    // When
+    var response = exportDeletedMarcIdsService.postExportDeletedMarcIds(null);
+    // Then
+    assertEquals(jobExecutionId, response.getJobExecutionId());
+    verify(marcDeletedIdsService).getFileDefinitionForMarcDeletedIds(null, null);
+    verify(dataExportService).postDataExport(exportRequestArgumentCaptor.capture());
+    var capturedRequest = exportRequestArgumentCaptor.getValue();
+    assertThat(capturedRequest.getFileDefinitionId()).isEqualTo(fileDefinitionId);
+    assertThat(capturedRequest.getJobProfileId())
+        .isEqualTo(UUID.fromString("6f7f3cd7-9f24-42eb-ae91-91af1cd54d0a"));
+    assertThat(capturedRequest.getAll()).isFalse();
+    assertThat(capturedRequest.getQuick()).isFalse();
+  }
 }
