5640e4371108bfd548fa76d8aa7857e1aefe6584
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2026-03-05 10:46:41 +0100

    # Code formatting issues

--- src/test/java/org/folio/dataexp/service/InsertExportIdServiceTest.java
+++ src/test/java/org/folio/dataexp/service/InsertExportIdServiceTest.java
@@ -2,6 +2,7 @@ package org.folio.dataexp.service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Collections;
@@ -15,7 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class InsertExportIdServiceTest {
@@ -34,22 +34,18 @@ class InsertExportIdServiceTest {
     verify(exportIdEntityRepository, never()).insertExportId(any(UUID.class), any(UUID.class));
   }
 
-    @Test
+  @Test
+  @TestMate(name = "TestMate-6eff45e9658ac662f28c81613fc61e9c")
   void saveBatchShouldCallInsertForEachEntityInList() {
-    // TestMate-6eff45e9658ac662f28c81613fc61e9c
     // Given
     var jobExecutionId1 = UUID.fromString("a1b2c3d4-1111-2222-3333-a1b2c3d4e5f6");
     var instanceId1 = UUID.fromString("f6e5d4c3-2222-1111-a1b2-f6e5d4c3b2a1");
     var jobExecutionId2 = UUID.fromString("b2c3d4e5-4444-5555-6666-b2c3d4e5f6a1");
     var instanceId2 = UUID.fromString("a1b2c3d4-5555-4444-b2c3-a1b2c3d4e5f6");
-    var entity1 = ExportIdEntity.builder()
-        .jobExecutionId(jobExecutionId1)
-        .instanceId(instanceId1)
-        .build();
-    var entity2 = ExportIdEntity.builder()
-        .jobExecutionId(jobExecutionId2)
-        .instanceId(instanceId2)
-        .build();
+    var entity1 =
+        ExportIdEntity.builder().jobExecutionId(jobExecutionId1).instanceId(instanceId1).build();
+    var entity2 =
+        ExportIdEntity.builder().jobExecutionId(jobExecutionId2).instanceId(instanceId2).build();
     var exportIds = List.of(entity1, entity2);
     // When
     insertExportIdService.saveBatch(exportIds);
