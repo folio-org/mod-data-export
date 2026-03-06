40183538fc54aace6aa058a11c76cc8b6f7fa5d8
FolioTestMate <Auto_EBS-FOL_TestMate@epam.com>
2026-03-06 11:04:28 +0300

    Pull request from unit_tests_org_folio_dataexp_service_batch_b3a9bc08_1 into master (#641)
    * [chore] [mod-data-export] unit tests
    
    * # Code formatting issues
    
    ---------
    
    Co-authored-by: aitestmate <ai_testmate@epam.com>
    Co-authored-by: Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>

--- src/test/java/org/folio/dataexp/service/InsertExportIdServiceTest.java
+++ src/test/java/org/folio/dataexp/service/InsertExportIdServiceTest.java
@@ -2,6 +2,7 @@ package org.folio.dataexp.service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Collections;
@@ -31,5 +32,26 @@ class InsertExportIdServiceTest {
     insertExportIdService.saveBatch(emptyList);
     // Then
     verify(exportIdEntityRepository, never()).insertExportId(any(UUID.class), any(UUID.class));
+  }
+
+  @Test
+  @TestMate(name = "TestMate-6eff45e9658ac662f28c81613fc61e9c")
+  void saveBatchShouldCallInsertForEachEntityInList() {
+    // Given
+    var jobExecutionId1 = UUID.fromString("a1b2c3d4-1111-2222-3333-a1b2c3d4e5f6");
+    var instanceId1 = UUID.fromString("f6e5d4c3-2222-1111-a1b2-f6e5d4c3b2a1");
+    var jobExecutionId2 = UUID.fromString("b2c3d4e5-4444-5555-6666-b2c3d4e5f6a1");
+    var instanceId2 = UUID.fromString("a1b2c3d4-5555-4444-b2c3-a1b2c3d4e5f6");
+    var entity1 =
+        ExportIdEntity.builder().jobExecutionId(jobExecutionId1).instanceId(instanceId1).build();
+    var entity2 =
+        ExportIdEntity.builder().jobExecutionId(jobExecutionId2).instanceId(instanceId2).build();
+    var exportIds = List.of(entity1, entity2);
+    // When
+    insertExportIdService.saveBatch(exportIds);
+    // Then
+    verify(exportIdEntityRepository, times(2)).insertExportId(any(UUID.class), any(UUID.class));
+    verify(exportIdEntityRepository).insertExportId(jobExecutionId1, instanceId1);
+    verify(exportIdEntityRepository).insertExportId(jobExecutionId2, instanceId2);
   }
 }
