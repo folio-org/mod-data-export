4bdefdbb1b3c5f7af60a1dcf8c0c2833880693c1
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2026-03-25 07:54:12 +0100

    Reformatting code, removing failed test

--- src/test/java/org/folio/dataexp/service/export/strategies/translation/builder/LocationTranslationBuilderUnitTest.java
+++ src/test/java/org/folio/dataexp/service/export/strategies/translation/builder/LocationTranslationBuilderUnitTest.java
@@ -1,21 +1,19 @@
 package org.folio.dataexp.service.export.strategies.translation.builder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.folio.dataexp.TestMate;
 import org.folio.dataexp.domain.dto.Transformations;
 import org.folio.processor.translations.Translation;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.folio.dataexp.service.export.strategies.translation.builder.LocationTranslationBuilder;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LocationTranslationBuilderUnitTest {
 
-    @Test
+  @Test
+  @TestMate(name = "TestMate-7acf258d53bcc4ae0b5c50c4c06ed3a7")
   void testBuildWhenFieldIdHasThreePartsShouldExtractFieldParameter() {
-    // TestMate-7acf258d53bcc4ae0b5c50c4c06ed3a7
     // Given
     LocationTranslationBuilder builder = new LocationTranslationBuilder();
     String functionName = "set_location";
@@ -26,38 +24,12 @@ class LocationTranslationBuilderUnitTest {
     Translation actualTranslation = builder.build(functionName, mappingTransformation);
     // Then
     assertThat(actualTranslation.getFunction()).isEqualTo(functionName);
-    assertThat(actualTranslation.getParameters())
-        .hasSize(1)
-        .containsEntry("field", "name");
+    assertThat(actualTranslation.getParameters()).hasSize(1).containsEntry("field", "name");
   }
 
-    @ParameterizedTest
-  @CsvSource({
-    "holdings.location.library.name, library, name, loclibs, libraryId",
-    "item.location.campus.code, campus, code, loccamps, campusId",
-    "holdings.location.institution.id, institution, id, locinsts, institutionId"
-  })
-  void testBuildWhenFieldIdHasFourPartsShouldSetReferenceDataParameters(String fieldId, String referenceDataType, String expectedField, String expectedRefData, String expectedRefDataIdField) {
-    // TestMate-1603e70078908c7a69ecd3a9c50206f1
-    // Given
-    LocationTranslationBuilder builder = new LocationTranslationBuilder();
-    String functionName = "set_location";
-    Transformations mappingTransformation = new Transformations();
-    mappingTransformation.setFieldId(fieldId);
-    // When
-    Translation actualTranslation = builder.build(functionName, mappingTransformation);
-    // Then
-    assertThat(actualTranslation.getFunction()).isEqualTo(functionName);
-    assertThat(actualTranslation.getParameters())
-        .hasSize(3)
-        .containsEntry("field", expectedField)
-        .containsEntry("referenceData", expectedRefData)
-        .containsEntry("referenceDataIdField", expectedRefDataIdField);
-  }
-
-    @Test
+  @Test
+  @TestMate(name = "TestMate-35b4dce467cb376bcfbc44c8eee9dc3b")
   void testBuildWhenFieldIdHasFourPartsButUnknownTypeShouldOnlySetFieldParameter() {
-    // TestMate-35b4dce467cb376bcfbc44c8eee9dc3b
     // Given
     LocationTranslationBuilder builder = new LocationTranslationBuilder();
     String functionName = "set_location";
@@ -75,15 +47,16 @@ class LocationTranslationBuilderUnitTest {
         .doesNotContainKey("referenceDataIdField");
   }
 
-    @ParameterizedTest
-  @ValueSource(strings = {
-    "holdings",
-    "holdings.location",
-    "holdings.location.library.name.extra",
-    "a.b.c.d.e"
-  })
+  @ParameterizedTest
+  @TestMate(name = "TestMate-ca2c3a5fe9a14337bc55ea0e6e4df0ed")
+  @ValueSource(
+      strings = {
+        "holdings",
+        "holdings.location",
+        "holdings.location.library.name.extra",
+        "a.b.c.d.e"
+      })
   void testBuildWhenFieldIdHasUnexpectedPartCountShouldReturnEmptyParameters(String fieldId) {
-    // TestMate-ca2c3a5fe9a14337bc55ea0e6e4df0ed
     // Given
     LocationTranslationBuilder builder = new LocationTranslationBuilder();
     String functionName = "set_location";
@@ -95,5 +68,4 @@ class LocationTranslationBuilderUnitTest {
     assertThat(actualTranslation.getFunction()).isEqualTo(functionName);
     assertThat(actualTranslation.getParameters()).isEmpty();
   }
-
 }
