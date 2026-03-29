74a38d89d73daaf3ec74c7f8cbb27ea7c3f9f61e
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2026-03-29 23:48:04 +0200

    Reformatting code, refactoring

--- src/test/java/org/folio/dataexp/service/export/strategies/RuleFactoryTest.java
+++ src/test/java/org/folio/dataexp/service/export/strategies/RuleFactoryTest.java
@@ -1,48 +1,43 @@
 package org.folio.dataexp.service.export.strategies;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.folio.dataexp.domain.dto.MappingProfile;
+import java.util.Optional;
+import org.folio.dataexp.TestMate;
+import org.folio.dataexp.domain.dto.RecordTypes;
+import org.folio.dataexp.domain.dto.Transformations;
+import org.folio.dataexp.exception.TransformationRuleException;
 import org.folio.processor.rule.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.Assertions.assertThat;
-import java.util.ArrayList;
-import java.util.Optional;
-import org.folio.dataexp.domain.dto.RecordTypes;
-import org.folio.dataexp.domain.dto.Transformations;
-import org.folio.dataexp.exception.TransformationRuleException;
 
 @ExtendWith(MockitoExtension.class)
 class RuleFactoryTest {
 
-    @Mock
-private List<Rule> defaultRulesFromConfigFile;
+  @Mock private List<Rule> defaultRulesFromConfigFile;
 
-    @Mock
-private List<Rule> defaultHoldingsRulesFromConfigFile;
+  @Mock private List<Rule> defaultHoldingsRulesFromConfigFile;
 
-    @InjectMocks
-private RuleFactory ruleFactory;
+  @InjectMocks private RuleFactory ruleFactory;
 
-    @Test
-void shouldReturnEmpty_whenRecordTypeIsNotInstance() throws TransformationRuleException {
-  // TestMate-d999471200a03a6a5b0d63efe04dbe53
-  // Given
-  Transformations transformations = new Transformations();
-  transformations.setEnabled(true);
-  transformations.setFieldId("holdings.hrid");
-  transformations.setRecordType(RecordTypes.HOLDINGS);
-  List<Rule> defaultRules = new ArrayList<>();
-  // When
-  Optional<Rule> result = ruleFactory.createDefaultByTransformations(transformations, defaultRules);
-  // Then
-  assertThat(result).isEmpty();
-}
-
+  @Test
+  @TestMate(name = "TestMate-d999471200a03a6a5b0d63efe04dbe53")
+  void shouldReturnEmpty_whenRecordTypeIsNotInstance() throws TransformationRuleException {
+    // Given
+    Transformations transformations = new Transformations();
+    transformations.setEnabled(true);
+    transformations.setFieldId("holdings.hrid");
+    transformations.setRecordType(RecordTypes.HOLDINGS);
+    List<Rule> defaultRules = new ArrayList<>();
+    // When
+    Optional<Rule> result =
+        ruleFactory.createDefaultByTransformations(transformations, defaultRules);
+    // Then
+    assertThat(result).isEmpty();
+  }
 }
