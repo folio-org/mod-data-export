2193740eac23997573f04ae0ab4f01c913d56b08
FolioTestMate <Auto_EBS-FOL_TestMate@epam.com>
2026-04-07 22:34:13 +0300

    Pull request from unit_tests_org_folio_dataexp_service_export_strategies_batch_95804c6d_1_6d6567 into master (#686)
    * [chore] [mod-data-export] unit tests
    
    * Reformatting code, refactoring
    
    * Reformatting code, refactoring
    
    ---------
    
    Co-authored-by: aitestmate <ai_testmate@epam.com>
    Co-authored-by: Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>

--- src/test/java/org/folio/dataexp/service/export/strategies/RuleFactoryTest.java
+++ src/test/java/org/folio/dataexp/service/export/strategies/RuleFactoryTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 import org.folio.dataexp.TestMate;
 import org.folio.dataexp.domain.dto.MappingProfile;
 import org.folio.dataexp.domain.dto.RecordTypes;
@@ -326,5 +327,69 @@ class RuleFactoryTest {
     var actualRules = spyRuleFactory.getRules(mappingProfile);
     // Then
     assertThat(actualRules).hasSize(2).containsExactly(rule100, rule245);
+  }
+
+  @Test
+  @TestMate(name = "TestMate-2825de9a93a8b8b2c1d3cec49562079e")
+  void buildRulesShouldCallCreateWhenMappingProfileDoesNotContainInstance()
+      throws TransformationRuleException {
+    // Given
+    var holdingsRule = new Rule();
+    holdingsRule.setId("holdings.hrid");
+    holdingsRule.setField("001");
+    var defaultRulesFromConfigFile = new ArrayList<Rule>();
+    var defaultHoldingsRulesFromConfigFile = List.of(holdingsRule);
+    var ruleFactoryLocal =
+        new RuleFactory(defaultRulesFromConfigFile, defaultHoldingsRulesFromConfigFile);
+    var mappingProfile = new MappingProfile();
+    mappingProfile.setRecordTypes(List.of(RecordTypes.HOLDINGS));
+    // When
+    var actualRules = ruleFactoryLocal.buildRules(mappingProfile);
+    // Then
+    assertThat(actualRules).containsExactly(holdingsRule);
+  }
+
+  @Test
+  @TestMate(name = "TestMate-60324df72f9fc56335d82b4890b76984")
+  void buildRulesShouldCallCreateWhenMappingProfileIsNull() throws TransformationRuleException {
+    // Given
+    var defaultRule = new Rule();
+    defaultRule.setId("instance.hrid");
+    defaultRule.setField("001");
+    var defaultRulesFromConfigFile = List.of(defaultRule);
+    var defaultHoldingsRulesFromConfigFile = new ArrayList<Rule>();
+    var ruleFactoryLocal =
+        new RuleFactory(defaultRulesFromConfigFile, defaultHoldingsRulesFromConfigFile);
+    // When
+    var actualRules = ruleFactoryLocal.buildRules(null);
+    // Then
+    assertThat(actualRules).containsExactly(defaultRule);
+  }
+
+  @Test
+  @TestMate(name = "TestMate-ff3b8fa3d9aa0238868a7fedc302cefe")
+  void buildRulesShouldCallCreateWhenRulesFromConfigIsEmpty() throws TransformationRuleException {
+    // Given
+    var instanceDefaultRule = new Rule();
+    instanceDefaultRule.setId("instance.default");
+    instanceDefaultRule.setField("001");
+    var holdingsDefaultRule = new Rule();
+    holdingsDefaultRule.setId("holdings.default");
+    holdingsDefaultRule.setField("002");
+    var defaultRulesFromConfigFile = List.of(instanceDefaultRule);
+    var defaultHoldingsRulesFromConfigFile = List.of(holdingsDefaultRule);
+    var ruleFactoryWithDefaults =
+        new RuleFactory(defaultRulesFromConfigFile, defaultHoldingsRulesFromConfigFile);
+    var mappingProfile = new MappingProfile();
+    mappingProfile.setId(UUID.fromString("f3f00482-936d-470a-819a-9769db382793"));
+    mappingProfile.setRecordTypes(List.of(RecordTypes.INSTANCE));
+    mappingProfile.setTransformations(new ArrayList<>());
+    // When
+    var actualRules = ruleFactoryWithDefaults.buildRules(mappingProfile);
+    // Then
+    assertThat(actualRules)
+        .hasSize(1)
+        .containsExactly(instanceDefaultRule)
+        .doesNotContain(holdingsDefaultRule);
   }
 }
